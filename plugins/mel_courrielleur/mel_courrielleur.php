55e023e61a5659564484f6b8cebf8b31f78ae243
Thomas Payen <thomas.payen@i-carre.net>
2019-05-16 17:41:42 +0200

    Plugin Mél Courrielleur

---
+++ plugins/mel_courrielleur/mel_courrielleur.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Plugin Melanie2 Courrielleur
+*
+* Skin courrielleur pour la task settings
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2
+* as published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program; if not, write to the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+class mel_courrielleur extends rcube_plugin
+{
+    /**
+     *
+     * @var string
+     */
+    public $task = 'settings|jappix|owncloud|ariane';
+    /**
+     * (non-PHPdoc)
+     * @see rcube_plugin::init()
+     */
+    function init()
+    {
+        if (isset($_GET['_courrielleur'])) {
+            // Ajout du css
+            $this->include_stylesheet('skins/courrielleur.css');
+            // Variable d'env courrielleur
+            rcmail::get_instance()->output->set_env('courrielleur', true);
+        }
+    }
+}

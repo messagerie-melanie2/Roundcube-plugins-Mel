758f3c53c68437ed35da5156b3ae15dde4e485e0
Thomas Payen <thomas.payen@i-carre.net>
2019-05-20 14:52:32 +0200

    Version file used by mel plugin

---
+++ version.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This class is used to have the version number of Roundcube Mél Webmail
+ *
+ * @author PNE Messagerie/Apitech
+ */
+class Version {
+
+    /**
+     * Version number
+     */
+    const VERSION = '1.4.7';
+   
+    /**
+     * Build
+     */
+    const BUILD = '20190430120052';
+
+}

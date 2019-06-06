1a78d2b08f2a354d2474b31265f0682d8a3a25f4
Thomas P <thomas.payen@i-carre.net>
2019-06-06 15:39:57 +0200

    Fix problem with output usage for mobile view button

--- plugins/mel_mobile/mel_mobile.php
+++ plugins/mel_mobile/mel_mobile.php
@@ -100,7 +100,9 @@ class mel_mobile extends rcube_plugin {
     $this->add_button(array('task' => 'switch_skin','data-ajax' => 'false', 'command' => 'switch_desktop','class' => 'button-switch_desktop ui-link ui-btn ui-corner-all ui-icon-desktop ui-btn-icon-left','innerclass' => 'button-inner','label' => 'mel_mobile.desktop'), 'taskbar_mobile');
     // Add the switch to mobile skin button
     if ($this->rc->config->get('skin') == 'mel_larry') {
-      $this->api->add_content(html::span('tablink', $this->rc->output->button(array('task' => 'switch_skin','command' => 'switch_mobile','class' => 'about-link','classsel' => 'about-link button-selected','innerclass' => 'button-inner','label' => 'mel_mobile.mobile'))), 'tabs');
+      if ($this->rc->task == 'settings' && $this->rc->output->type == 'html') {
+        $this->api->add_content(html::span('tablink', $this->rc->output->button(array('task' => 'switch_skin','command' => 'switch_mobile','class' => 'about-link','classsel' => 'about-link button-selected','innerclass' => 'button-inner','label' => 'mel_mobile.mobile'))), 'tabs');
+      }
     }
     else {
       $this->add_button(array('task' => 'switch_skin','command' => 'switch_mobile','class' => 'about-link','classsel' => 'about-link button-selected','innerclass' => 'button-inner','label' => 'mel_mobile.mobile'), 'topline-left');

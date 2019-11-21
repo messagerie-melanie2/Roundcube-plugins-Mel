14a64c31d5dcdc3f94debf4a7df75c65ba2b6925
Thomas P <thomas.payen@i-carre.net>
2019-11-21 13:34:31 +0100

    Support search in aide page

--- aide/index.php
+++ aide/index.php
@@ -19,6 +19,17 @@ require_once 'config.php';
   <div id="prevcontent">
     <h1>Aide Mél</h1>
     <div id="content">
+    	<?php 
+        if (isset($config['search'])) {
+      ?>
+      <div id="help-search">
+      	<span class="help-search-label">Recherche rapide dans la documentation : </span>
+      	<span class="help-search-input"><input type="text" placeholder="Saisissez des mots clés" onkeyup="search(event, this);"></span>
+      </div>
+      <div id="help-search-results"></div>
+      <?php 
+        }
+      ?>    
       <div id="help-url-list">
         <ul>
         <?php
@@ -64,3 +75,80 @@ require_once 'config.php';
   </div>
 </body>
 </html>
+<?php 
+if (isset($config['search'])) {
+?>
+<script type="text/javascript">
+  var _search = <?= json_encode($config['search']) ?>;
+  var handle;
+  
+  function search(event, object) {
+    var results = {};
+    document.getElementById("help-search-results").innerHTML = "";
+    document.getElementById("help-search-results").style = "display: none;";
+    if (object.value.length > 3) {
+      if (handle) {
+        clearTimeout(handle);
+      }
+    	handle = setTimeout(function() {
+        //var value = object.value.toLowerCase();
+        for (const value of object.value.toLowerCase().split(' ')) {
+          value.trim();
+          if (value.length > 3) {
+            for (const key in _search) {
+              var keywords = _search[key].keywords;
+              for (const k in _search[key].keywords) {
+                if (_search[key].keywords[k].indexOf(value) !== -1) {
+                  if (results[key]) {
+                    results[key]++;
+                  }
+                  else {
+                    results[key] = 1;
+                  }
+                }
+              }
+            }
+          }
+        }
+        
+        if (results) {
+          var _res = [];
+       		// Trier les résultats
+          for (const key in results) {
+            _res.push({key: key, value: results[key]});
+          }
+          _res.sort((a, b) => (a.value < b.value) ? 1 : -1)
+          document.getElementById("help-search-results").style = "display: block;";
+          var i = 0;
+          for (const r of _res) {
+            if (i++ > 4)  {
+              break;
+            }
+            var title = document.createElement('span');
+            title.className = "title";
+            title.textContent = _search[r.key].title;
+            var description = document.createElement('span');
+            description.className = "description";
+            description.textContent = _search[r.key].description;
+            var numbers = document.createElement('span');
+            numbers.className = "numbers";
+            numbers.textContent = "(" + r.value + " résultat(s))";
+            var url = document.createElement('a');
+            url.href = _search[r.key].url;
+            url.target = "_blank";
+            url.appendChild(title);
+            url.appendChild(numbers);
+            url.appendChild(description);
+            var result = document.createElement('div');
+            result.className = "result";
+            result.appendChild(url);
+            document.getElementById("help-search-results").appendChild(result);
+          }
+        }
+    	}, 350);
+    }
+  }
+</script>
+<?php 
+}
+?>

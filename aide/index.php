8da6916da9f7a18543b8a46e0bba9fdc62393050
Thomas P <thomas.payen@i-carre.net>
2019-11-21 15:27:52 +0100

    Optimization for search index

--- aide/index.php
+++ aide/index.php
@@ -23,7 +23,7 @@ require_once 'config.php';
         if (isset($config['search'])) {
       ?>
       <div id="help-search">
-      	<span class="help-search-label">Recherche rapide dans la documentation : </span>
+      	<span class="help-search-label">Recherche rapide dans la documentation (bêta) : </span>
       	<span class="help-search-input"><input type="text" placeholder="Saisissez des mots clés" onkeyup="search(event, this);"></span>
       </div>
       <div id="help-search-results"></div>
@@ -79,26 +79,38 @@ require_once 'config.php';
 if (isset($config['search'])) {
 ?>
 <script type="text/javascript">
+	<?php 
+	$index = [];
+	foreach ($config['search'] as $k => $s) {
+	  foreach ($s['keywords'] as $word) {
+	    if (isset($index[$word])) {
+	      if (!in_array($k, $index[$word])) {
+	        $index[$word][] = $k;
+	      }
+	    }
+	    else {
+	      $index[$word] = [$k];
+	    }
+	  }
+	}
+	?>
   var _search = <?= json_encode($config['search']) ?>;
+  var _index = <?= json_encode($index) ?>;
   var handle;
   
   function search(event, object) {
     var results = {};
-    document.getElementById("help-search-results").innerHTML = "";
-    document.getElementById("help-search-results").style = "display: none;";
     if (object.value.length > 3) {
       if (handle) {
         clearTimeout(handle);
       }
     	handle = setTimeout(function() {
-        //var value = object.value.toLowerCase();
-        for (const value of object.value.toLowerCase().split(' ')) {
-          value.trim();
-          if (value.length > 3) {
-            for (const key in _search) {
-              var keywords = _search[key].keywords;
-              for (const k in _search[key].keywords) {
-                if (_search[key].keywords[k].indexOf(value) !== -1) {
+      	var values = object.value.toLowerCase().split(' ');
+        for (const word in _index) {
+          for (const value of values) {
+            if (value.length > 3) {
+              if (word.indexOf(value) !== -1) {
+                for (const key of _index[word]) {
                   if (results[key]) {
                     results[key]++;
                   }
@@ -110,8 +122,7 @@ if (isset($config['search'])) {
             }
           }
         }
-        
-        if (results) {
+        if (Object.keys(results).length) {
           var _res = [];
        		// Trier les résultats
           for (const key in results) {
@@ -119,9 +130,10 @@ if (isset($config['search'])) {
           }
           _res.sort((a, b) => (a.value < b.value) ? 1 : -1)
           document.getElementById("help-search-results").style = "display: block;";
+          document.getElementById("help-search-results").innerHTML = "";
           var i = 0;
           for (const r of _res) {
-            if (i++ > 4)  {
+            if (i++ > 5)  {
               break;
             }
             var title = document.createElement('span');
@@ -145,7 +157,15 @@ if (isset($config['search'])) {
             document.getElementById("help-search-results").appendChild(result);
           }
         }
-    	}, 350);
+        else {
+          document.getElementById("help-search-results").style = "display: block; text-align: center;";
+          document.getElementById("help-search-results").textContent = "Pas de résultat, essayez de saisir plus de mots clés.";
+        }
+    	}, 300);
+    }
+    else {
+      document.getElementById("help-search-results").innerHTML = "";
+      document.getElementById("help-search-results").style = "display: none;";
     }
   }
 </script>

19da1d892cf7dffe776f4751623c9de8fee63445
Thomas Payen <thomas.payen@i-carre.net>
2019-12-18 15:15:42 +0100

    New plugin mel_portail

--- plugins/mel_portail/mel_portail.php
+++ plugins/mel_portail/mel_portail.php
@@ -47,12 +47,12 @@ class mel_portail extends rcube_plugin
     if ($this->rc->task == 'portail') {
       // Chargement de la conf
       $this->load_config();
+      // Ajout de l'interface
+      include_once 'imodule.php';
       // Index
       $this->register_action('index', array($this, 'action'));
       // Ajout du css
       $this->include_stylesheet($this->local_skin_path() . '/mel_portail.css');
-      // Ajout le javascript
-      $this->include_script('mel_portail.js');
       // Add handler
       $this->rc->output->add_handler('portail_items_list', array($this, 'items_list'));
     }
@@ -72,16 +72,53 @@ class mel_portail extends rcube_plugin
     $content = "";
     $scripts_js = [];
     $scripts_css = [];
+    $user_infos = LibMelanie\Ldap\Ldap::GetUserInfos($this->rc->get_user_name());
     $items = $this->rc->config->get('portail_items_list', []);
+    $templates = $this->rc->config->get('portail_templates_list', []);
     foreach ($items as $id => $item) {
+      if (!isset($templates[$item['type']])) {
+        unset($items[$id]);
+        continue;
+      }
+      $template = $templates[$item['type']];
+      // Check if the item match the dn
+      if (isset($item['dn'])) {
+        if (is_string($item['dn']) && strpos($user_infos['dn'], $item['dn']) === false) {
+          unset($items[$id]);
+          continue;
+        }
+        else if (is_array($item['dn'])) {
+          $found = false;
+          foreach ($item['dn'] as $dn) {
+            if (strpos($user_infos['dn'], $dn) !== false) {
+              $found = true;
+            }
+          }
+          if (!$found) {
+            unset($items[$id]);
+            continue;
+          }
+        }
+      }
+      // Ajoute le php ?
+      if (isset($template['php'])) {
+        include_once 'modules/' . $item['type'] . '/' . $template['php'];
+        $classname = ucfirst($item['type']);
+        $object = new $classname($id);
+        if (!$object->show()) {
+          unset($items[$id]);
+          continue;
+        }
+        $object->init();
+      }
       $content .= $this->item_html(['id' => $id], $item);
       // Ajoute le javascript ?
-      if (isset($item['js'])) {
-        $scripts_js['modules/' . $item['type'] . '/' . $item['js']] = true;
+      if (isset($template['js'])) {
+        $scripts_js['modules/' . $item['type'] . '/' . $template['js']] = true;
       }
       // Ajoute le css ?
-      if (isset($item['css'])) {
-        $scripts_css['modules/' . $item['type'] . '/' . $item['css']] = true;
+      if (isset($template['css'])) {
+        $scripts_css['modules/' . $item['type'] . '/' . $template['css']] = true;
       }
     }
     // Charger les scripts JS
@@ -98,6 +135,8 @@ class mel_portail extends rcube_plugin
     }
     
     $this->rc->output->set_env("portail_items", $items);
+    // Ajout le javascript
+    $this->include_script('mel_portail.js');
     return html::tag('section', $attrib, $content);
   }
   
@@ -118,33 +157,80 @@ class mel_portail extends rcube_plugin
     else {
       $flip = "";
     }
-    // Header html du front
-    $header = html::tag('header', [],
-        $flip .
-        html::a(['href' => $item['url'], 'onclick' => $item['onclick'] ?: null], 
-            html::img(['class' => 'icon', 'alt' => $item['name'] . ' logo', 'src' => isset($item['logo']) ? $item['logo'] : '/plugins/mel_portail/modules/' . $item['type'] . '/logo.png']) .
-            html::tag('h1', 'title', $item['name']) .
-            html::tag('p', 'description', $item['description'])
-        )
-    );
+    $logo_style = null;
+    if (isset($item['logobg'])) {
+      $logo_style = 'background-color: '.$item['logobg'].';
+                     border-radius: 5px;
+                  	-webkit-box-shadow: 0 3px 0 0 rgba(43,43,43,.35);
+                  	-moz-box-shadow: 0 3px 0 0 rgba(43,43,43,.35);
+                  	box-shadow: 0 3px 0 0	rgba(43,43,43,.35);';
+    }
+    if (isset($item['url'])) {
+      // Header html du front
+      $header = html::tag('header', [],
+          $flip .
+          html::a(['href' => $item['url'], 'target' => $item['newtab'] ? '_blank': null, 'onclick' => $item['onclick'] ?: null],
+              html::img(['class' => 'icon', 'style' => $logo_style, 'alt' => $item['name'] . ' logo', 'src' => isset($item['logo']) ? $item['logo'] : '/plugins/mel_portail/modules/' . $item['type'] . '/logo.png']) .
+              html::tag('h1', 'title', $item['name']) .
+              html::tag('p', 'description', $item['description'])
+              )
+          );
+    }
+    else {
+      // Header html du front
+      $header = html::tag('header', [],
+          $flip .
+            html::tag('h1', 'title', $item['name'])
+          );
+    }
+    
     // Gestion des boutons
     $buttons = "";
+    $buttons_back = "";
     if (isset($item['buttons'])) {
       $buttons_list = "";
+      $buttons_back_list = "";
       foreach ($item['buttons'] as $text => $button) {
-        $buttons_list .= html::a(['class' => 'button', 'href' => $button['href'] ?: '#', 'onclick' => $button['onclick'] ?: null], $text);
+        if (isset($button['back']) && $button['back']) {
+          $buttons_back_list .= html::a(['class' => 'button', 'href' => $button['href'] ?: '#', 'target' => $item['newtab'] && isset($button['href']) ? '_blank': null, 'onclick' => $button['onclick'] ?: null], $text);
+        }
+        else {
+          $buttons_list .= html::a(['class' => 'button', 'href' => $button['href'] ?: '#', 'target' => $item['newtab'] && isset($button['href']) ? '_blank': null, 'onclick' => $button['onclick'] ?: null], $text);
+        }
+        
       }
       $buttons = html::div('buttons', $buttons_list);
+      if (strlen($buttons_back_list)) {
+        $buttons_back = html::div('buttons', $buttons_back_list);
+      }
+      if ($item['type'] == 'fluxrss') {
+        $attrib['class'] .= ' nonewsfront';
+      }
+    }
+    else if (isset($item['links'])) {
+      $attrib['class'] .= " links";
+      $links_list = "";
+      foreach ($item['links'] as $text => $link) {
+        $links_list .= html::a(['class' => 'link', 'href' => $link['url'], 'target' => $item['newtab'] ? '_blank': null, 'onclick' => $link['onclick'] ?: null],
+            html::img(['class' => 'icon', 'style' => $logo_style, 'alt' => $item['name'] . ' logo', 'src' => isset($link['logo']) ? $link['logo'] : '/plugins/mel_portail/modules/' . $item['type'] . '/logo.png']) .
+            html::tag('h3', 'title', $text)
+        );
+      }
+      $buttons = html::div('links', $links_list);
+    }
+    else if ($item['type'] == 'website' || $item['type'] == 'communication') {
+      $attrib['class'] .= " full";
     }
     // Générer le contenu html
     if ($item['flip']) {
       // Front + back
-      $content = html::tag('article', 'front', $header . $buttons) .
-                  html::tag('article', 'back', html::tag('header', [], $flip));
+      $content = html::tag('article', ['class' => 'front', 'title' => $item['title'] ?: null], $header . $buttons) .
+                  html::tag('article', 'back', html::tag('header', [], $flip) . $buttons_back);
     }
     else {
       // Front
-      $content = html::tag('article', 'front', $header . $buttons);
+      $content = html::tag('article', ['class' => 'front', 'title' => $item['title'] ?: null], $header . $buttons) .
+                  html::tag('article', 'back blank', '');
     }
     
     return html::tag('article', $attrib, $content);
--- plugins/mel_portail/modules/webconf/script.js
+++
@@ -1,7 +0,0 @@
-function joinWebConf() {
-	var conf = prompt("Quelle conférence souhaitait vous rejoindre ?", "ExempleDeConference365");
-	if (conf) {
-		//rcmail.portail_open_url(rcmail.env.portail_items.webconf.url + conf);
-		window.location = rcmail.env.portail_items.webconf.url + conf.split('/').pop();
-	}
-}

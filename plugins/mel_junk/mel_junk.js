8e58f453d7e59129532d313eb8989964ee6a114a
Thomas P <thomas.payen@i-carre.net>
2020-04-22 18:17:23 +0200

    new plugin mel_junk to send spam to admin

---
+++ plugins/mel_junk/mel_junk.js
@@ -0,0 +1,81 @@
+/**
+ * Plugin Mél Junk
+ *
+ * Permet de rediriger un pourriel à l'administrateur, 
+ * se base sur les plugins bounce et markasjunk
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+if (window.rcmail) {
+    rcmail.addEventListener('init', function(evt) {
+        // register command (directly enable in message view mode)
+        rcmail.register_command('plugin.mel_junk', mel_junk_box);
+        rcmail.register_command('plugin.mel_junk_send', mel_junk_send);
+
+        // add event-listener to message list
+        if (rcmail.message_list) {
+            rcmail.message_list.addEventListener('select', function(list) {
+                rcmail.enable_command('plugin.mel_junk', (list.get_selection().length > 0 || rcmail.env.uid) && rcmail.env.mailbox != rcmail.env.junk_folder);
+                rcmail.enable_command('plugin.mel_junk_send', (list.get_selection().length > 0 || rcmail.env.uid) && rcmail.env.mailbox != rcmail.env.junk_folder);
+                rcmail.enable_command('plugin.markasjunk', (list.get_selection().length > 0 || rcmail.env.uid) && rcmail.env.mailbox != rcmail.env.junk_folder);
+            });
+        }
+    });
+}
+
+// Open mel junk box
+function mel_junk_box(prop, item, event) {
+    UI.toggle_popup('mel_junk-box', event);
+}
+
+// mel junk action
+function mel_junk_send(prop, item, event) {
+    if ($('#mel_junk-box').is(':visible')) {
+        // Quels sont les choix de l'utilisateur dans le popup ?
+        var send_admin = $('#mel_junk-box #mel_junk_send_admin').is(':checked');
+        var junk_folder = $('#mel_junk-box #mel_junk_folder').is(':checked');
+    }
+    else {
+        // Si le popup est masqué c'est une action directe
+        var send_admin = true;
+        var junk_folder = true;
+    }
+    // Send message to administrator ? (bounce plugin)
+    if (send_admin) {      
+        // Récupère les uid de messages sélectionnés
+        var uids = rcmail.env.uid ? [rcmail.env.uid] : rcmail.message_list.get_selection();
+        if (uids.length === 0) {
+            return false;
+        }
+        for (const uid of uids) {
+            // all checks passed, send message
+            lock = rcmail.set_busy(true, 'sendingmessage');
+            rcmail.http_post('plugin.bounce', 
+                                '_uid='+uid+
+                                '&_to='+urlencode(rcmail.env.administrator_email)+
+                                '&_cc=&_bcc='+
+                                '&_mbox='+urlencode(rcmail.env.mailbox), 
+                                lock);
+        }
+    }
+    // Put message in junk folder ? (markasjunk plugin)
+    if (junk_folder) {
+        rcmail_markasjunk();
+    }
+    if ($('#mel_junk-box').is(':visible')) {
+        $('#mel_junk-box').hide();
+    }
+    return true;
+}
---
+++ plugins/mel_junk/mel_junk.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Plugin Mél Junk
+ *
+ * Permet de rediriger un pourriel à l'administrateur, 
+ * se base sur les plugins bounce et markasjunk
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+class mel_junk extends rcube_plugin
+{
+    /**
+     *
+     * @var string
+     */
+    public $task = 'mail';
+
+    /**
+     * Initialisation du plugin
+     * 
+     * @see rcube_plugin::init()
+     */
+    function init() {
+        // Plugins require
+        $this->require_plugin('bounce');
+        $this->require_plugin('markasjunk');
+
+        $rcmail = rcmail::get_instance();
+
+        if ($rcmail->task == 'mail' && ($rcmail->action == '' || $rcmail->action == 'show')) {
+            $this->load_config();
+            $this->include_script('mel_junk.js');
+            $this->add_texts('localization', true);
+            $this->add_hook('render_page', array($this, 'render_box'));
+            $rcmail->output->set_env('administrator_email', $rcmail->config->get('mel_junk_administrator_email'));
+            $rcmail->output->set_env('junk_folder', $rcmail->config->get('junk_mbox'));
+        }
+    }
+
+    /**
+     * Junk box
+     */
+    public function render_box($p) {
+        $this->add_texts('localization');
+        $rcmail = rcmail::get_instance();
+    
+        $attrib = [
+            'id'    => 'mel_junk-box',
+            'class' => 'popupmenu',
+            'data-sticky' => 'true',
+        ];
+    
+        $button = new html_inputfield(array('type' => 'button'));
+        $checkbox = new html_checkbox();
+    
+        $rcmail->output->add_footer(html::div($attrib,
+          $rcmail->output->form_tag(array('name' => 'mel_junkform', 'method' => 'post', 'action' => './', 'enctype' => 'multipart/form-data'),
+            html::tag('input', array('type' => "hidden", 'name' => '_action', 'value' => 'mel_junk')) .
+              html::div('mel_junk-title', rcube_utils::rep_specialchars_output($this->gettext('title'), 'html', 'strict')) .
+            html::div('junk-body',
+              html::div('checkboxes', 
+                html::div('junk_folder', $checkbox->show('junk_folder', ['id' => 'mel_junk_folder', 'class' => 'junk_folder', 'value' => 'junk_folder']) . html::label('mel_junk_folder', $this->gettext('add to junk folder'))) .
+                html::div('send_admin', $checkbox->show('send_admin', ['id' => 'mel_junk_send_admin', 'class' => 'send_admin', 'value' => 'send_admin']) . html::label('mel_junk_send_admin', $this->gettext('send message to administrator')))
+              ) .
+              html::div('buttons',
+                $button->show(rcube_utils::rep_specialchars_output($this->gettext('junk'), 'html', 'strict'), array('class' => 'button mainaction',
+                      'onclick' => rcmail_output::JS_OBJECT_NAME . ".command('plugin.mel_junk_send', this.mel_junkform)")) . ' ' .
+                $button->show($rcmail->gettext('close'), array('class' => 'button', 'onclick' => "UI.toggle_popup('$attrib[id]', event)"))
+                  
+              )
+            )
+          )
+        ));
+        $rcmail->output->add_gui_object('mel_junkbox', $attrib['id']);
+        $rcmail->output->add_gui_object('mel_junkform', 'mel_junkform');
+    
+        if ($rcmail->config->get('ismobile', false)) {
+          $this->include_stylesheet('skins/mel_larry_mobile/junk.css');
+        }
+        else {
+          $this->include_stylesheet($this->local_skin_path() . '/junk.css');
+        }
+    }
+}

c676b414950b8414c22d24d2898ed9b51c9ba6d0
Thomas P <thomas.payen@i-carre.net>
2020-04-22 09:30:22 +0200

    Archivage headers files

--- plugins/mel_archivage/mel_archivage.js
+++ plugins/mel_archivage/mel_archivage.js
@@ -1,3 +1,23 @@
+/**
+ * Plugin Mél Archivage
+ *
+ * Plugin d'archivage des messages depuis Roundcube
+ * Les messages sont téléchargés sur le poste de l'utilisateur
+ * Puis copié dans un dossier configuré dans 'mel_archivage_folder' 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 if (window.rcmail) {
     if (rcmail.env.task == 'mail') {
         rcmail.addEventListener('responseafterlist', function(evt) {

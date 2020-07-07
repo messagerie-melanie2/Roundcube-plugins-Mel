35d2b8c99b6c65379a25200ff73f30265bdce303
Thomas P <thomas.payen@i-carre.net>
2020-07-07 11:42:12 +0200

    New version 1.4.8.3

--- version.php
+++ version.php
@@ -9,10 +9,10 @@ class Version {
   /**
    * Version number
    */
-  const VERSION = '1.4.8.1';
+  const VERSION = '1.4.8.3';
   
   /**
    * Build
    */
-  const BUILD = '20200611091252';
+  const BUILD = '20200707114152';
 }

f57f38f39702334831eea5642d93d56953d00662
Thomas P <thomas.payen@i-carre.net>
2021-01-21 20:24:47 +0100

    0006033: Plugin de génération d'une signature ministériel

---
+++ plugins/mel_signatures/mel_signatures.php
@@ -0,0 +1,157 @@
+<?php
+/**
+ * Plugin Mél Signatures
+*
+* Permet de générer des signatures officielles
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2
+* as published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program; if not, write to the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+class mel_signatures extends rcube_plugin
+{
+    /**
+     *
+     * @var string
+     */
+    public $task = 'settings';
+    /**
+     *
+     * @var rcmail
+     */
+    private $rc;
+    /**
+     * Directions list
+     */
+    private $directions = ['SG' => 'Secrétariat Général'];
+
+    /**
+     * (non-PHPdoc)
+     * @see rcube_plugin::init()
+     */
+    function init()
+    {
+        $this->rc = rcmail::get_instance();
+
+        if ($this->rc->task == 'settings') {
+            // Chargement de la conf
+            $this->load_config();
+
+            $this->add_texts('localization/', ['mobilephone', 'phonephone', 'signaturecopied', 'clictocopy']);
+            
+            $this->add_hook('settings_actions', array($this, 'settings_actions'));
+            $this->api->register_action('plugin.mel_signatures', $this->ID, array(
+              $this,
+              'settings'
+            ));
+        }
+    }
+
+    /**
+     * Adds Signatures section in Settings
+     */
+    function settings_actions($args)
+    {
+        $args['actions'][] = array(
+            'action' => 'plugin.mel_signatures',
+            'class'  => 'signatures',
+            'label'  => 'task',
+            'domain' => 'mel_signatures',
+            'title'  => 'signaturestitle',
+        );
+        return $args;
+    }
+
+    /**
+     * Show settings template
+     */
+    function settings() {
+        // Chargement des informations de l'utilisateur
+        $user = driver_mel::gi()->getUser();
+        $fields = ['name', 'lastname', 'firstname', 'service', 'title', 'street', 'postalcode', 'locality', 'phonenumber', 'mobilephone', 'roomnumber', ];
+        $user->load($fields);
+        if (isset($user->lastname)) {
+            $user->name = $user->firstname . " " . $user->lastname;
+        }
+        foreach ($fields as $field) {
+            $value = $user->$field;
+            // Gestion du service
+            if ($field == 'service' && !empty($value)) {
+                $tmp = explode('/', $value, 2);
+                $this->rc->output->set_env('department', $this->directions[$tmp[0]] ?: $tmp[0]);
+                $this->rc->output->set_env($field, $tmp[1]);
+            }
+            else if ($field == 'roomnumber' && !empty($value) && strpos($value, $this->gettext('roomnumber')) === false) {
+                $this->rc->output->set_env('roomnumber', $this->gettext('roomnumber') . $value);
+            }
+            else {
+                $this->rc->output->set_env($field, $value);
+            }
+        }
+        // Ajout du css
+        $this->include_stylesheet($this->local_skin_path() . '/signatures.css');
+        // Ajout du JS
+        $this->include_script('signature.js');
+        $this->rc->output->add_handlers(array(
+            'signaturelinks'  => array($this, 'links'),
+            'signaturelogo'   => array($this, 'logo'),
+        ));
+        // Chargement du template d'affichage
+        $this->rc->output->set_pagetitle($this->gettext('title'));
+        $this->rc->output->send('mel_signatures.settings');
+    }
+
+    /**
+     * Handler pour le choix du logo
+     */
+    function logo($attrib) {
+        if (empty($attrib['id']))
+            $attrib['id'] = 'input-logo';
+
+        $select = new html_select($attrib);
+        $sources = [];
+        foreach ($this->rc->config->get('signature_images', []) as $name => $link) {
+            $select->add($name, $link);
+            // Format the image SRC:  data:{mime};base64,{data};
+            $sources[$link] = 'data: '.mime_content_type(__DIR__.'/'.$link).';base64,'.base64_encode(file_get_contents(__DIR__.'/'.$link));
+        }
+        $this->rc->output->set_env('logo_sources', $sources);
+        return $select->show();
+    }
+
+    /**
+     * Handler pour la liste des liens
+     */
+    function links($attrib) {
+        if (empty($attrib['id']))
+            $attrib['id'] = 'input-links';
+
+        $attrib['class'] = "dropdown-check-list";
+        
+        $links = "";
+        $env_links = [];
+        $checkbox = new html_checkbox();
+        $i = 1;
+        foreach ($this->rc->config->get('signature_links', []) as $name => $link) {
+            $id = "signature_links_$i";
+            $i++;
+            $env_links[$link] = $name;
+            $links .= html::tag('li', [], $checkbox->show("https://www.ecologie.gouv.fr", ['value' => $link, 'id' => $id, 'onchange' => 'onInputChange();']) . html::label(['for' => $id], $name));
+        }
+        $this->rc->output->set_env('signature_links', $env_links);
+        return html::div($attrib,
+            html::span(['class' => 'anchor'], $this->gettext('linksselection')) .
+            html::tag('ul', ['class' => 'items'], $links)
+        );
+    }
+}
---
+++ plugins/mel_signatures/signature.js
@@ -0,0 +1,134 @@
+var inputTimeout = null;
+
+window.onload = function() {
+    onInputChange();
+
+    var checkList = document.getElementById('input-links');
+    checkList.getElementsByClassName('anchor')[0].onclick = function(evt) {
+        if (checkList.classList.contains('visible'))
+            checkList.classList.remove('visible');
+        else
+            checkList.classList.add('visible');
+    }
+};
+
+/**
+ * On input change event refresh signature html
+ */
+function onInputChange() {
+    if (inputTimeout) {
+        clearTimeout(window.inputTimeout);
+    }
+    window.inputTimeout = setTimeout(() => {
+        let html = getSignatureHTML();
+        document.querySelector("#settings-right .usersignature .sign").innerHTML = html;
+        document.getElementById("signature_html").value = html;
+    }, 300);
+}
+
+/**
+ * Copier la signature dans le presse papier
+ */
+function copy_signature() {
+    document.querySelector("#signature_html").select();
+    let success = document.execCommand("copy");
+    if (success) {
+        document.querySelector(".action-button").value = rcmail.get_label('mel_signatures.signaturecopied');
+        setTimeout(() => {
+            document.querySelector(".action-button").value = rcmail.get_label('mel_signatures.clictocopy');
+        }, 1000);
+    }
+}
+
+/**
+ * Generate HTML signature from template HTML
+ * 
+ * return HTML
+ */
+function getSignatureHTML() {
+    let signature_html = document.getElementById("signature_template").innerHTML;
+    // User name
+    signature_html = signature_html.replace('%%TEMPLATE_NAME%%', document.getElementById("input-nom").value);
+
+    // User job
+    let jobtitle = document.getElementById("input-fonction").value;
+    signature_html = signature_html.replace('%%TEMPLATE_JOBTITLE%%', jobtitle ? jobtitle + '<br>' : '');
+
+    // User address
+    let address = document.getElementById("input-address").value;
+    signature_html = signature_html.replace('%%TEMPLATE_ADDRESS%%', address ? address + '<br>' : '');
+
+    // User office
+    let office = document.getElementById("input-bureau").value;
+    signature_html = signature_html.replace('%%TEMPLATE_OFFICE%%', office ? office + '<br>' : '');
+
+    // Phone
+    let phone = document.getElementById("input-fixe").value;
+    let mobile = document.getElementById("input-mobile").value;
+    phone = formatPhoneNumber(phone);
+    document.getElementById("input-fixe").value = phone;
+    mobile = formatPhoneNumber(mobile);
+    document.getElementById("input-mobile").value = mobile;
+    if (phone && mobile) {
+        signature_html = signature_html.replace('%%TEMPLATE_PHONE%%', rcmail.get_label('mel_signatures.phonephone') + phone + ' - ' + rcmail.get_label('mel_signatures.mobilephone') + mobile);
+    }
+    else if (phone) {
+        signature_html = signature_html.replace('%%TEMPLATE_PHONE%%', rcmail.get_label('mel_signatures.phonephone') + phone);
+    }
+    else if (mobile) {
+        signature_html = signature_html.replace('%%TEMPLATE_PHONE%%', rcmail.get_label('mel_signatures.mobilephone') + mobile);
+    }
+    else {
+        signature_html = signature_html.replace('%%TEMPLATE_PHONE%%', document.getElementById("input-fixe").value);
+    }
+
+    // Service
+    let service = document.getElementById("input-service").value;
+    signature_html = signature_html.replace('%%TEMPLATE_SERVICE%%', service ? service + '<br>' : '');
+    signature_html = signature_html.replace(/%%TEMPLATE_DIRECTION%%/g, document.getElementById("input-department").value);
+
+    // Gestion des liens
+    let checkboxes = document.querySelectorAll("#input-links .items input");
+    let links = "";
+    for (const checkbox of checkboxes) {
+        if (checkbox.checked) {
+            let a = document.createElement('a');
+            a.style = "color:#000000;font-size:8pt;font-family: Arial,sans-serif; font-weight : bold;";
+            a.href = checkbox.value;
+            a.innerText = rcmail.env.signature_links[checkbox.value];
+            links += a.outerHTML + '<br>';
+        }
+    }
+    signature_html = signature_html.replace('%%TEMPLATE_LINKS%%', links);
+
+    let logo = document.createElement('img');
+    let select = document.getElementById("input-logo");
+    logo.src = rcmail.env.logo_sources[select.value];
+    logo.alt = select.options[select.selectedIndex].text;
+    signature_html = signature_html.replace('%%TEMPLATE_LOGO%%', logo.outerHTML);
+    
+    return signature_html;
+}
+
+/**
+ * Formatte le numéro de téléphone au format français
+ */
+function formatPhoneNumber(number) {
+    let indicatifList = ['+33', '+262', '+269', '+508', '+590', '+594', '+596', '+681', '+687', '+689'];
+    if (number.indexOf('+') === 0 && number.indexOf(' ') === -1) {
+        for (const iterator of indicatifList) {
+            if (number.indexOf(iterator) === 0) {
+                number = number.replace(iterator, '');
+                const first = number.substr(0, 1);
+                number = number.substr(1);
+                number = number.replace(/(.{2})/g, "$1 ");
+                number = iterator + " " + first + " " + number;
+                break;
+            }
+        }
+    }
+    else if (number.length === 10 && number.indexOf('0') === 0) {
+        number = number.replace(/(.{2})/g,"$1 ");
+    }
+    return number.trim();
+}

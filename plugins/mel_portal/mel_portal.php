5f5298ebaac052eb255b311c0f210e745385705e
Rotomeca <delphin.tommy@gmail.com>
2021-05-25 11:01:10 +0200

    correction des traces

--- plugins/mel_portal/mel_portal.php
+++ plugins/mel_portal/mel_portal.php
@@ -138,6 +138,7 @@ class mel_portal extends rcube_plugin
               $this->add_module($config[$pageName]["modules"][$i], $object->item_html(), $object->row_size());
           } catch (\Throwable $th) { //$th->getMessage()
               mel_logs::get_instance()->log(mel_logs::ERROR, "[mel_portal->load_modules] Un erreur est survenue pour le module ".$config[$pageName]["modules"][$i]." !");
+              mel_logs::get_instance()->log(mel_logs::ERROR, "###[mel_portal->load_modules]".$th->getTraceAsString());
               mel_logs::get_instance()->log(mel_logs::ERROR, "###[mel_portal->load_modules]".$th->getMessage());
             }
       }

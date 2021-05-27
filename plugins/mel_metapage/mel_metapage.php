f4bb18a2a2028bd0fe1bbeab9da9fc7998292b3e
Rotomeca <delphin.tommy@gmail.com>
2021-05-27 10:41:44 +0200

    style maquette + corrections

--- plugins/mel_metapage/mel_metapage.php
+++ plugins/mel_metapage/mel_metapage.php
@@ -47,7 +47,8 @@ class mel_metapage extends rcube_plugin
         // Récupération de l'instance de rcmail
         $this->rc = rcmail::get_instance();
 
-        if (rcube_utils::get_input_value('_framed', rcube_utils::INPUT_GET) === "1")
+        if (rcube_utils::get_input_value('_framed', rcube_utils::INPUT_GET) === "1"
+        || rcube_utils::get_input_value('_extwin', rcube_utils::INPUT_GET) === "1")
             return;
 
         if ($this->rc->task !== "login" && $this->rc->task !== "logout")

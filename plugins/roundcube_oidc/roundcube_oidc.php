c10b62ecbfc7943a20429c18592479555a0a3ae1
TBG-FR <tom-brian.garcia@apitech.fr>
2021-06-24 11:39:40 +0200

    Plugin roundcube_oidc : Code and config modifications to improve and adapt the plugin to our usage (no clear password, Kerberos, LDAP call, hooks)

--- plugins/roundcube_oidc/roundcube_oidc.php
+++ plugins/roundcube_oidc/roundcube_oidc.php
@@ -3,134 +3,186 @@
 // Require composer autoload for direct installs
 @include __DIR__ . '/vendor/autoload.php';
 
-use Jumbojett\OpenIDConnectClient;
+// Include the helper class (wrapping Jumbojett\OpenIDConnectClient)
+@include __DIR__ . '/cerbere_token_helper.php';
+
+// Include Mel ORM library
+@include_once 'includes/libm2.php';
+
+/**
+ * Roundcube OIDC
+ *
+ * Login to roundcube with OpenID Connect provider
+ *
+ * @license	MIT License: <http://opensource.org/licenses/MIT>
+ * @author Varun Patil
+ * @category  Plugin for RoundCube WebMail
+ * 
+ * -----------------------------------
+ * 
+ * @author Tom-Brian GARCIA
+ * Modifications :
+ *      - Avoid getting or transmitting (clear) password
+ *      - Get and transmit the id_token to SASL
+ *      - Rewrited the plugin to use hooks (with add_hook)
+ *      - Added Kerberos support (and LDAP call)
+ */
+class roundcube_oidc extends rcube_plugin
+{
+    private $redirect_query;
+    private $tokenHelper;
 
     /**
-     * Roundcube OIDC
-     *
-     * Login to roundcube with OpenID Connect provider
-     *
-     * @license	MIT License: <http://opensource.org/licenses/MIT>
-     * @author Varun Patil
-     * @category  Plugin for RoundCube WebMail
+     * Plugin initialization
      */
-    class roundcube_oidc extends rcube_plugin
+    function init()
     {
-        public $task = 'login|logout';
-        private $map;
+        // Load plugin's config file
+        $this->load_config();
 
-        function init() {
-            $this->load_config('config.inc.php.dist');
-            $this->load_config('config.inc.php');
-            $this->add_hook('template_object_loginform', array($this, 'loginform'));
-        }
+        // Add plugin's functions to Roundcube
+        $this->add_hook('startup', array($this, 'startup'));
+        $this->add_hook('authenticate', array($this, 'authenticate'));
+        $this->add_hook('login_after', array($this, 'login_after'));
+    }
 
-        function altReturn($ERROR) {
-            // Get mail object
-            $RCMAIL = rcmail::get_instance();
+    /**
+     * Startup hook handler
+     */
+    function startup($args)
+    {
+        if(empty($_SESSION['user_id']))
+        {
+            // Get Roundcube instance
+            $rcmail = rcmail::get_instance();
+            $auth = false;
 
-            // Check if overridden login page
-            $altLogin = $RCMAIL->config->get('oidc_login_page');
+            //region =========== AUTH methods ===========
 
-            // Include and exit
-            if (isset($altLogin) && !empty($altLogin)) {
-                include $altLogin;
-                exit;
-            }
-        }
+            // Auth OIDC triggered by Kerberos (with LDAP field)
+            if($rcmail->config->get('oidc_auth_kerberos') && !empty($_SERVER['REMOTE_USER']))
+            {
+                mel_logs::get_instance()->log(mel_logs::INFO, "Connexion OIDC avec déclenchement Kerberos/LDAP...");
+                mel_logs::get_instance()->log(mel_logs::DEBUG, "Connection OIDC avec déclenchement Kerberos/LDAP...");
 
-        public function loginform($content) {
-            // Add the login link
-            $content['content'] .= "<p> <a href='?oidc=1'> Login with OIDC </a> </p>";
-
-            // Check if we are starting or resuming oidc auth
-            if (!isset($_GET['code']) && !isset($_GET['oidc'])) {
-                $this->altReturn(null);
-                return $content;
-            }
-
-            // Define error for alt login
-            $ERROR = '';
-
-            // Get mail object
-            $RCMAIL = rcmail::get_instance();
-
-            // Get master password and default imap server
-            $password = $RCMAIL->config->get('oidc_imap_master_password');
-            $imap_server = $RCMAIL->config->get('default_host');
-
-            // Build provider
-            $oidc = new OpenIDConnectClient(
-                $RCMAIL->config->get('oidc_url'),
-                $RCMAIL->config->get('oidc_client'),
-                $RCMAIL->config->get('oidc_secret')
-            );
-            $oidc->addScope($RCMAIL->config->get('oidc_scope'));
-
-            // Get user information
-            try {
-                $oidc->authenticate();
-                $user = json_decode(json_encode($oidc->requestUserInfo()), true);
-            } catch (\Exception $e) {
-                $ERROR = 'OIDC Authentication Failed <br/>' . $e->getMessage();
-                $content['content'] .= "<p class='alert-danger'> $ERROR </p>";
-                $this->altReturn($ERROR);
-                return $content;
-            }
-
-            // Parse fields
-            $uid = $user[$RCMAIL->config->get('oidc_field_uid')];
-            $password = get($user[$RCMAIL->config->get('oidc_field_password')], $password);
-            $imap_server = get($user[$RCMAIL->config->get('oidc_field_server')], $imap_server);
-
-            // Check if master user is present
-            $master = $RCMAIL->config->get('oidc_config_master_user');
-            if ($master != '') {
-                $uid .= $RCMAIL->config->get('oidc_master_user_separator') . $master;
-            }
-
-            // Trigger auth hook
-            $auth = $RCMAIL->plugins->exec_hook('authenticate', array(
-                'user' => $uid,
-                'pass' => $password,
-                'cookiecheck' => true,
-                'valid'       => true,
-            ));
-
-            // Login to IMAP
-            if ($RCMAIL->login($auth['user'], $password, $imap_server, $auth['cookiecheck'])) {
-                $RCMAIL->session->remove('temp');
-                $RCMAIL->session->regenerate_id(false);
-                $RCMAIL->session->set_auth_cookie();
-                $RCMAIL->log_login();
-                $query = array();
-                $redir = $RCMAIL->plugins->exec_hook('login_after', $query + array('_task' => 'mail'));
-                unset($redir['abort'], $redir['_err']);
-                $query = array('_action' => '');
-                $OUTPUT = new rcmail_html_page();
-                $redir = $RCMAIL->plugins->exec_hook('login_after', $query + array('_task' => 'mail'));
-                $RCMAIL->session->set_auth_cookie();
-
-                // Update user profile
-                $iid = $RCMAIL->user->get_identity()['identity_id'];
-                $claim_name = $user['name'];
-                if (isset($iid) && isset($claim_name)) {
-                    $RCMAIL->user->update_identity($iid, array('name' => $claim_name));
+                mel_logs::get_instance()->log(mel_logs::INFO, "Valeur du champ REMOTE_USER (Kerberos) :" . $_SERVER['REMOTE_USER']);
+                mel_logs::get_instance()->log(mel_logs::DEBUG, "Valeur du champ REMOTE_USER (Kerberos) :" . $_SERVER['REMOTE_USER']);
+                
+                // Transformation de 'prenom.nom@REALM' en 'prenom.nom' seulement 
+                $user_uid = explode("@",$_SERVER['REMOTE_USER'])[0];
+                
+                // Récupération de l'utilisateur
+                $user = driver_mel::get_instance()->getUser($user_uid);
+    
+                // Chargement de l'attribut du LDAP
+                $user->load('cerbere');
+    
+                mel_logs::get_instance()->log(mel_logs::INFO, "Valeur du champ AUTH.Cerbere pour $user_uid : $user->cerbere");
+                mel_logs::get_instance()->log(mel_logs::DEBUG, "Valeur du champ AUTH.Cerbere pour $user_uid : $user->cerbere");
+    
+                // Si l'utilisateur est autorisé à se connecter par OIDC Cerbère
+                if($user->cerbere != null && $user->cerbere >= 1)
+                {
+                    mel_logs::get_instance()->log(mel_logs::INFO, "Tentative de connexion OIDC Cerbère pour l'utilisateur $user_uid après vérification Kerberos et LDAP.");
+                    
+                    $auth = true;
                 }
+            }
+            // Auth OIDC only (without LDAP field)
+            else if($rcmail->config->get('oidc_auth_standalone') && isset($_GET['oidc']))
+            {
+                mel_logs::get_instance()->log(mel_logs::INFO, "Connexion OIDC direct...");
+                mel_logs::get_instance()->log(mel_logs::DEBUG, "Connection OIDC direct...");
 
-                $OUTPUT->redirect($redir, 0, true);
-            } else {
-                $ERROR = 'IMAP authentication failed!';
-                $content['content'] .= "<p class='alert-danger'> $ERROR </p>";
+                $auth = true;
             }
 
-            $this->altReturn($ERROR);
-            return $content;
+            //endregion =================================
+
+            //region ========== AUTH execution ==========
+
+            mel_logs::get_instance()->log(mel_logs::INFO, "Connection déclenchée : $auth");
+                    
+            if($auth)
+            {
+                // Trigger login action
+                $args['action'] = 'login';
+    
+                // Setup a new TokenHelper (wrapping OIDC)
+                $this->tokenHelper = new CerbereTokenHelper(
+                    $rcmail->config->get('oidc_url'),
+                    $rcmail->config->get('oidc_client'),
+                    $rcmail->config->get('oidc_secret'),
+                    explode(" ", $rcmail->config->get('oidc_scope')),
+                    false, // host verification
+                    false // peer verification
+                );
+
+                // Store the redirection query
+                $this->redirect_query = $_SERVER['QUERY_STRING'];
+            }
+
+            //endregion =================================
         }
 
+        return $args;
     }
 
-    function get(&$var, $default=null) {
-        return isset($var) ? $var : $default;
+    /**
+     * Authenticate hook handler
+     */
+    function authenticate($args)
+    {
+        if($this->tokenHelper != null)
+        {
+            // Get Roundcube
+            $rcmail = rcmail::get_instance();
+            
+            try
+            {
+                // Get user information
+                $user = json_decode(json_encode($this->tokenHelper->getUserInfo()), true);
+
+                // Parse fields
+                $uid = $user[$rcmail->config->get('oidc_field_uid')];
+
+                // Get modified ID token
+                // $password = $this->tokenHelper->modifyTokenID(
+                //     $this->tokenHelper->getToken(TokenTypeEnum::ID_TOKEN), $user['uid'], ModificationTypeEnum::APPEND_CONTENT/*_JSON*/, false, false
+                // );
+
+                $password = $this->tokenHelper->getToken(TokenTypeEnum::ID_TOKEN);
+            }
+            catch (\Exception $e)
+            {
+                // TODO improve
+                echo 'OIDC Authentication Failed <br/>' . $e->getMessage();
+                mel_logs::get_instance()->log(mel_logs::ERROR, "OIDC Authentication Failed <br/>" . $e->getMessage());
+            }
+            
+            // Modify args values
+            $args['user'] = $uid;
+            $args['pass'] = $password;
+            $args['cookiecheck'] = true;
+            $args['valid'] = true;
+        }
+
+        return $args;
     }
+
+    /**
+     * Post-login hook handler
+     */
+    function login_after($args)
+    {
+        // Redirect to the previous QUERY_STRING
+        if ($this->redirect_query)
+        {
+            header('Location: ./?' . $this->redirect_query);
+            exit;
+        }
+
+        return $args;
+    }
+}
 

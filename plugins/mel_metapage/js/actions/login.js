8ce79456f8eb5df357704431dbb393579914f6da
Rotomeca <delphin.tommy@gmail.com>
2021-12-07 15:41:57 +0100

    Correction du login (dm) et du chat (dm)

--- plugins/mel_metapage/js/actions/login.js
+++ plugins/mel_metapage/js/actions/login.js
@@ -2,3 +2,22 @@ if (parent != window)
 {
     parent.location.reload();
 }
+
+$(document).ready(() => {
+    
+    let color_mode = window.matchMedia('(prefers-color-scheme: dark)');
+    color_mode.addListener(function(e) {
+        let _color_mode = e.matches ? 'dark' : 'light';
+
+        if (MEL_ELASTIC_UI.color_mode() !== _color_mode) MEL_ELASTIC_UI.switch_color();
+
+        if (_color_mode === 'dark') $(".mel-logo").attr("src", 'skins/mel_elastic/images/taskbar-logo.svg');
+        else $(".mel-logo").attr("src", 'plugins/mel_portal/skins/elastic/images/logoportal.svg');
+        // switch_color_mode();
+        // reset_cookie();
+    });
+
+    if (MEL_ELASTIC_UI.color_mode() === 'dark') $(".mel-logo").attr("src", 'skins/mel_elastic/images/taskbar-logo.svg')
+    else $(".mel-logo").attr("src", 'plugins/mel_portal/skins/elastic/images/logoportal.svg');
+    
+});

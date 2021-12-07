0d32aebf41a06329b2245a40f9d2fccad65a2e9d
Thomas P <thomas.payen@i-carre.net>
2021-12-07 19:31:33 +0100

    Merge request MCE #2
    Theme generique

--- plugins/mel_larry/mel_larry.php
+++ plugins/mel_larry/mel_larry.php
@@ -36,7 +36,7 @@ class mel_larry extends rcube_plugin
   const CSS_FOLDER = 'css/';
   /**
    * Liste des thèmes supportés par la skin mel_larry
-   * 
+   *
    * @var array
    */
   const THEMES = ['auto', 'light', 'dark', 'sunset'];
@@ -89,7 +89,6 @@ class mel_larry extends rcube_plugin
   public static $tasks_js_map = [
       'tasks' => 'tasklist.js',
   ];
-
   /**
    * Initialisation du plugin
    * @see rcube_plugin::init()
@@ -101,7 +100,7 @@ class mel_larry extends rcube_plugin
     $this->add_hook('startup', array($this, 'startup'));
 
     // Config hook
-    if ($rcmail->config->get('skin') == 'mel_larry') {
+    if ($rcmail->config->get('skin') == static::SKIN_NAME) {
       $this->add_hook('config_get', array($this,'config_get'));
       // Theme preference configuration
       $this->add_hook('preferences_list',             array($this, 'prefs_list'));
@@ -155,23 +154,23 @@ class mel_larry extends rcube_plugin
           'name' => $this->gettext("settings_themes"),
           'options' => [],
         ]];
-        
+
         $input    = new html_radiobutton(array('name'=>'_themes'));
         $field_id = 'rcmfd_theme';
-        foreach (self::THEMES as $theme) {
-          $thumbnail   = "plugins/mel_larry/images/themes/$theme.png";
+        foreach (static::THEMES as $theme) {
+          $thumbnail   = "plugins/" . static::SKIN_NAME . "/images/themes/$theme.png";
           $themename    = $this->gettext("themes_title_$theme");
           $themedescription    = $this->gettext("themes_description_$theme");
-  
+
           $img = html::img(array(
                   'src'     => $thumbnail,
                   'class'   => 'themethumbnail',
                   'alt'     => $themename,
                   'onerror' => "this.src = rcmail.assets_path('program/resources/blank.gif')",
           ));
-  
+
           $themes[$key]['options'][$theme]['content'] = html::label(array('class' => 'themeselection'),
-              html::span('themeitem', $input->show($rc->config->get('mel_larry_theme', 'auto'), array('value' => $theme, 'id' => $field_id.$theme))) .
+              html::span('themeitem', $input->show($rc->config->get(static::SKIN_NAME . '_theme', 'auto'), array('value' => $theme, 'id' => $field_id.$theme))) .
               html::span('themeitem', $img) .
               html::span('themeitem', html::span('themename', rcube::Q($themename)) . html::br() .
                   html::span('themedescription', $themedescription))
@@ -186,10 +185,10 @@ class mel_larry extends rcube_plugin
   /**
    * Insert at
    */
-  private function array_insert(&$array, $position, $insert_array) {
+  protected function array_insert(&$array, $position, $insert_array) {
     $first_array = array_splice ($array, 0, $position);
     $array = array_merge ($first_array, $insert_array, $array);
-  } 
+  }
 
   /**
    * Handler for user preferences save (preferences_save hook)
@@ -201,7 +200,7 @@ class mel_larry extends rcube_plugin
       $dont_override = (array)$rc->config->get('dont_override', array());
       $key = 'themes';
       if (!in_array($key, $dont_override)) {
-        $config_key = 'mel_larry_theme';
+        $config_key = static::SKIN_NAME . '_theme';
         $value = rcube_utils::get_input_value('_' . $key, rcube_utils::INPUT_POST);
         if ($rc->config->get($config_key, 'auto') != $value) {
           $rc->output->command('reload', 500);
@@ -215,54 +214,54 @@ class mel_larry extends rcube_plugin
   /**
    * Change From message header
    */
-  function mail_headers($args) {
-    if (isset($args['output']['from']) 
+  public function mail_headers($args) {
+    if (isset($args['output']['from'])
         && $args['output']['from']['html']) {
       $args['output']['from']['value'] = $this->rcmail_address_string($args['output']['from']['raw'], null, true, "/images/addcontact.png");
     }
-        
+
     return $args;
   }
 
   /**
    * Decode address string and re-format it as HTML links
    */
-  private function rcmail_address_string($input, $max=null, $linked=false, $addicon=null, $default_charset=null, $title=null)
+  protected function rcmail_address_string($input, $max=null, $linked=false, $addicon=null, $default_charset=null, $title=null)
   {
     global $PRINT_MODE;
-    
+
     $rcmail = rcmail::get_instance();
     $a_parts = rcube_mime::decode_address_list($input, null, true, $default_charset);
-    
+
     if (!count($a_parts)) {
       return $input;
     }
-    
+
     $c   = count($a_parts);
     $j   = 0;
     $out = '';
     $allvalues  = array();
     $show_email = $rcmail->config->get('message_show_email');
-    
+
     if ($addicon && !isset($_SESSION['writeable_abook'])) {
       $_SESSION['writeable_abook'] = $rcmail->get_address_sources(true) ? true : false;
     }
-    
+
     foreach ($a_parts as $part) {
       $j++;
-      
+
       $name   = $part['name'];
       $mailto = $part['mailto'];
       $string = $part['string'];
       $valid  = rcube_utils::check_email($mailto, false);
-      
+
       // IDNA ASCII to Unicode
       if ($name == $mailto)
         $name = rcube_utils::idn_to_utf8($name);
         if ($string == $mailto)
           $string = rcube_utils::idn_to_utf8($string);
           $mailto = rcube_utils::idn_to_utf8($mailto);
-          
+
           if ($PRINT_MODE) {
             $address = sprintf('%s &lt;%s&gt;', rcube::Q($name), rcube::Q($mailto));
           }
@@ -274,21 +273,21 @@ class mel_larry extends rcube_plugin
                   'onclick' => sprintf("return %s.command('compose','%s',this)",
                     rcmail_output::JS_OBJECT_NAME, rcube::JQ(format_email_recipient($mailto, $name))),
               );
-              
+
               $attrs['title'] = $mailto;
               if (!$name) {
                 $name = explode('@', $mailto, 2);
                 $name = ucfirst(str_replace('.', ' ', $name[0])) . ' (' . $name[1] . ')';
               }
               $content =  html::tag('span', 'name', rcube::Q($name)) . html::tag('span', 'mailto', rcube::Q($mailto));
-              
+
               $address = html::a($attrs, $content);
             }
             else {
               $address = html::span(array('title' => $mailto, 'class' => "rcmContactAddress"),
                 rcube::Q($name ?: $mailto));
             }
-            
+
             if ($addicon && $_SESSION['writeable_abook']) {
               $address .= html::a(array(
                   'href'    => "#add",
@@ -310,14 +309,14 @@ class mel_larry extends rcube_plugin
               $address = trim($address . ' ' . rcube::Q($name ? sprintf('<%s>', $mailto) : $mailto));
             }
           }
-          
+
           $address = html::span('adr', $address);
           $allvalues[] = $address;
-          
+
           if (!$moreadrs) {
             $out .= ($out ? ', ' : '') . $address;
           }
-          
+
           if ($max && $j == $max && $c > $j) {
             if ($linked) {
               $moreadrs = $c - $j;
@@ -328,10 +327,10 @@ class mel_larry extends rcube_plugin
             }
           }
     }
-    
+
     if ($moreadrs) {
       $label = rcube::Q($rcmail->gettext(array('name' => 'andnmore', 'vars' => array('nr' => $moreadrs))));
-      
+
       if ($PRINT_MODE) {
         $out .= ' ' . html::a(array(
             'href'    => '#more',
@@ -351,7 +350,7 @@ class mel_larry extends rcube_plugin
         ), $label);
       }
     }
-    
+
     return $out;
   }
 
@@ -368,30 +367,30 @@ class mel_larry extends rcube_plugin
     // Add localization
     $this->add_texts('localization/', true);
     // Check if the user use mel_larry skin
-    if ($skin == self::SKIN_NAME && !$rc->output->get_env('mobile')) {
+    if ($skin == static::SKIN_NAME && !$rc->output->get_env('mobile')) {
       // Themes css
-      $theme_param = $rc->config->get('mel_larry_theme', 'auto');
-      $this->include_stylesheet(self::CSS_FOLDER.str_replace('%%param%%', $theme_param, self::THEMES_CSS));
+      $theme_param = $rc->config->get(static::SKIN_NAME . '_theme', 'auto');
+      $this->include_stylesheet(static::CSS_FOLDER.str_replace('%%param%%', $theme_param, static::THEMES_CSS));
       // App css
-      $this->include_stylesheet(self::CSS_FOLDER.self::APP_CSS);
+      $this->include_stylesheet(static::CSS_FOLDER.static::APP_CSS);
       // For each plugin, add the associated css file
-      foreach(self::$plugins_css_map as $plugin_name => $css_file) {
+      foreach(static::$plugins_css_map as $plugin_name => $css_file) {
         if (in_array($plugin_name, $plugins)) {
-          $this->include_stylesheet(self::CSS_FOLDER.$css_file);
+          $this->include_stylesheet(static::CSS_FOLDER.$css_file);
         }
       }
       // Add the associated task css file
-      if (isset(self::$tasks_css_map[$rc->task])) {
-        $this->include_stylesheet(self::CSS_FOLDER.self::$tasks_css_map[$rc->task]);
+      if (isset(static::$tasks_css_map[$rc->task])) {
+        $this->include_stylesheet(static::CSS_FOLDER.static::$tasks_css_map[$rc->task]);
       }
       // Load other custom css files
-      $this->include_stylesheet(self::CSS_FOLDER.self::$plugins_css_map['jqueryui']);            
+      $this->include_stylesheet(static::CSS_FOLDER.static::$plugins_css_map['jqueryui']);
       // Load ui & mel js file
-      $this->include_script(self::JS_FOLDER.self::UI_JS);
-      $this->include_script(self::JS_FOLDER.self::MEL_JS);
+      $this->include_script(static::JS_FOLDER.static::UI_JS);
+      $this->include_script(static::JS_FOLDER.static::MEL_JS);
       // Add the associated task js file
-      if (isset(self::$tasks_js_map[$rc->task])) {
-        $this->include_script(self::JS_FOLDER.self::$tasks_js_map[$rc->task]);
+      if (isset(static::$tasks_js_map[$rc->task])) {
+        $this->include_script(static::JS_FOLDER.static::$tasks_js_map[$rc->task]);
       }
       // Add plugins list
       $rc->output->set_env('plugins', $rc->config->get('plugins', []));

a305543a653c47d5f34e0360e683652fbfd5e8ea
Rotomeca <delphin.tommy@gmail.com>
2021-12-14 10:24:28 +0100

    Suppression de code obsolète

--- plugins/rocket_chat/rocket_chat_storage_event.js
+++ plugins/rocket_chat/rocket_chat_storage_event.js
@@ -1,15 +1,15 @@
-if (window.rcmail) {
-	rcmail.addEventListener('init', function(evt) {
-		// Récupération du title rocket
-		var rocket_chat_title = localStorage.getItem('rocket_chat_title');
-		if (rocket_chat_title) {
-			$('.button-rocket_chat .button-inner').text(rocket_chat_title);
-		}
-	});
-//	
-//	window.addEventListener('storage', function(e) {
-//		if (e.key == 'rocket_chat_title') {
-//			$('.button-rocket_chat .button-inner').text(e.newValue);
-//		}
-//	});
-}
+// if (window.rcmail) {
+// 	rcmail.addEventListener('init', function(evt) {
+// 		// Récupération du title rocket
+// 		var rocket_chat_title = localStorage.getItem('rocket_chat_title');
+// 		if (rocket_chat_title) {
+// 			$('.button-rocket_chat .button-inner').text(rocket_chat_title);
+// 		}
+// 	});
+// //	
+// //	window.addEventListener('storage', function(e) {
+// //		if (e.key == 'rocket_chat_title') {
+// //			$('.button-rocket_chat .button-inner').text(e.newValue);
+// //		}
+// //	});
+// }

67e2dd20a96747c3c53d1d2a75f65eb235eba29c
Thomas P <thomas.payen@i-carre.net>
2022-02-11 15:50:08 +0100

    Fix previous
    Dans le cas des occurrences modifiés elles sont quand meme dans EXDATE (sic)

--- plugins/calendar/drivers/mel/mel_driver.php
+++ plugins/calendar/drivers/mel/mel_driver.php
@@ -1409,21 +1409,32 @@ class mel_driver extends calendar_driver {
             $master = $this->_read_postprocess($_event);
             $recurrence_date = rcube_utils::anytodatetime(date("Y-m-d H:i:s", $_recurrence_date), $master['start']->getTimezone());
 
-            // Rechercher si on est pas sur une EXDATE
+            // Initialise l'event id avec le recurrence_date
+            if (!isset($event['id'])) {
+              $event['id'] = $event['uid'] . '@DATE-' . $_recurrence_date;
+            }
+
+            // Rechercher si on est pas sur une EXDATE (exception et pas occurrence)
             if (isset($master['recurrence']) 
                 && isset($master['recurrence']['EXDATE'])
                 && is_array($master['recurrence']['EXDATE'])) {
+
+              // Traiter les occurrences à part (plus tard)
+              if (isset($master['recurrence']) 
+                  && isset($master['recurrence']['EXCEPTIONS'])) {
+                $exceptions = $master['recurrence']['EXCEPTIONS'];
+              }
+              else {
+                $exceptions = [];
+              }
+
+              // Gérer les exceptions qui ne sont pas des occurrences
               foreach ($master['recurrence']['EXDATE'] as $exdate) {
-                if ($exdate == $recurrence_date) {
+                if ($exdate == $recurrence_date && !isset($exceptions[$event['id']])) {
                   // Il s'agit d'une occurrence supprimée le get_event doit retourner false
                   return false;
                 }
               }
-            }
-
-            // Initialise l'event id avec le recurrence_date
-            if (!isset($event['id'])) {
-              $event['id'] = $event['uid'] . '@DATE-' . $_recurrence_date;
             }
 
             // Est-ce qu'il s'agit d'une occurrence ?

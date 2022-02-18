2b4ce5b6666d15078b3d69431d32e2ad6c72c097
Thomas P <thomas.payen@i-carre.net>
2022-02-18 19:09:35 +0100

    0006480: La double auth ne fonctionne pas correctement

--- plugins/mel_doubleauth/mel_doubleauth.js
+++ plugins/mel_doubleauth/mel_doubleauth.js
@@ -12,112 +12,141 @@ if (window.rcmail) {
 				}
 				
 				rcmail.addEventListener('responseafterplugin.mel_doubleauth-removeuser', function(evt) {
-					$('#2FA_activate_button').removeAttr('disabled');
-					$('.settingsbox table tr.added').remove();
+					$('#p2FA_activate_button').removeAttr('disabled');
+					$('#mel_doubleauth-form fieldset.main div.table div.row.added').remove();
 					window.double_fact_saved = true;
 				});
 
 				rcmail.addEventListener('responseafterplugin.mel_doubleauth-adduser', function(evt) {
-						$('#2FA_secret').get(0).value = b32_encode(evt.response.code);
+					// Return bootstrap row
+					function row(html, className = 'row added') {
+						let div = document.createElement('div');
+						div.className = className;
+						div.innerHTML = html;
+						return div;
+					}
+
+					// Return bootstrap col
+					function col(html, className = 'col-sm my-auto') {
+						let div = document.createElement('div');
+						div.className = className;
+						div.innerHTML = html;
+						return div.outerHTML;
+					}
+
+					// Return cercle span
+					function cercle(text) {
+						let span = document.createElement('span');
+						span.className = 'cercle';
+						span.textContent = text;
+						return span.outerHTML;
+					}
+
+					let secret = b32_encode(evt.response.code);
+					let table = document.querySelector('#mel_doubleauth-form fieldset.main div.table');
+
+					document.querySelector('#p2FA_secret').value = secret;
 	
-						console.log($('#2FA_secret').get(0).value);
+					// 2/ Open FreeOTP indication
+					table.append(row(
+						col(cercle(2), 'col-sm-1 my-auto') +
+						col(rcmail.gettext('action_2', 'mel_doubleauth') + 
+							'<div id="img_qr_code" style="display: visible;"></div>')
+					));
+
+					// 3/ Input secret
+					table.append(row(
+						col(cercle(3), 'col-sm-1 my-auto') +
+						col(rcmail.gettext('action_3', 'mel_doubleauth') + 
+							'&nbsp;&nbsp;<input type="text" class="form-control"  size="32" id="2FA_code_input" value="' + secret + '">')
+					));
+
+					// Show QR Code 
+					table.append(row(
+						col('&nbsp;', 'col-sm-1') +
+						col('<div id="2FA_qr_code" style="display: inline-block; margin-top: 10px; padding: 10px; background: #fff;"></div>')
+					));
+
+					// 4/ Code checker
+					table.append(row(
+						col(cercle(4), 'col-sm-1 my-auto') +
+						col(rcmail.gettext('action_4', 'mel_doubleauth') + 
+							'&nbsp;&nbsp;<input type="text" class="form-control" id="2FA_code_to_check" maxlength="10" onkeypress="if (event.keyCode == 13) return false;">' + 
+							'&nbsp;&nbsp;<input type="button" class="button mainaction btn btn-primary" id="2FA_check_code" value="' + rcmail.gettext('check_code', 'mel_doubleauth') + '"/>')
+					));
+
+					// 5/ Save indication
+					table.append(row(
+						col(cercle(5), 'col-sm-1 my-auto') +
+						col(rcmail.gettext('action_5', 'mel_doubleauth'))
+					));
+
+					// Save button
+					table.append(row(
+						col('&nbsp;', 'col-sm-1 my-auto') +
+						col('<input type="button" class="button mainaction 2FA_save btn btn-primary" onclick="return rcmail.command(\'plugin.mel_doubleauth-save\',\'\',this,event)" value="Enregistrer" disabled="disabled">')
+					));
 	
-						$('table tr:last')
-								.before(
-										"<tr class=\"added\"><td width='20px'><span class='cercle'>2</span></td><td colspan='2'>"
-												+ rcmail.gettext('action_2',
-														'mel_doubleauth')
-												+ '<div id="img_qr_code" style="display: visible; "></div></td></tr>');
-						// add qr-code before msg_infor
-						var url_qr_code_values = 'otpauth://totp/'
-								+ $('#prefs-title').html().split(/ - /)[1]
-								+ '?secret=' + $('#2FA_secret').get(0).value
-								+ '&issuer=M2Web';
-						$('table tr:last')
-								.before(
-										'<tr class=\"added\"><td width="20px"><span class="cercle">3</span></td><td colspan="2">'
-												+ rcmail.gettext('action_3',
-														'mel_doubleauth') + '&nbsp;&nbsp;<input type="text" size="32" id="2FA_code_input" value="' + $('#2FA_secret').get(0).value + '">'
-												+ '</tr><tr class=\"added\"><td></td><td colspan="2"><div id="2FA_qr_code" style="display: inline-block; margin-top: 10px; padding: 10px; background: #fff;"></div></td></tr>');
-	
-						$('table tr:last')
-								.before(
-										"<tr class=\"added\"><td width='20px'><span class='cercle'>4</span></td><td colspan='2'>"
-												+ rcmail.gettext('action_4',
-														'mel_doubleauth')
-												+ '&nbsp;&nbsp;<input type="text" id="2FA_code_to_check" maxlength="10" onkeypress="if (event.keyCode == 13) return false;">&nbsp;&nbsp;<input type="button" class="button mainaction" id="2FA_check_code" value="'
-												+ rcmail.gettext('check_code',
-														'mel_doubleauth') + '"></td></tr>');
-	
-						$('table tr:last').before(
-								"<tr class=\"added\"><td width='20px'><span class='cercle'>5</span></td><td colspan='2'>"
-										+ rcmail.gettext('action_5', 'mel_doubleauth')
-										+ '</td></tr>');
-						$('table tr:last')
-								.before(
-										'<tr class=\"added\"><td colspan="3"><p><input type="button" class="button mainaction 2FA_save" id="rcmbtn120" onclick="return rcmail.command(\'plugin.mel_doubleauth-save\',\'\',this,event)" value="Enregistrer" disabled="disabled"></p></td></tr>');
-	
-						// ajax
-						$('#2FA_check_code')
-								.click(
-										function() {
-											url = "./?_action=plugin.mel_doubleauth-checkcode&code="
-													+ $('#2FA_code_to_check').val();
-											$.post(url, function(data) {
-												alert(data);
-												if (data == rcmail.gettext('code_ok',
-														'mel_doubleauth'))
-													$('.2FA_save').removeAttr('disabled');
-	
-											});
-										});
-	
-						var qrcode = new QRCode(document
-								.getElementById("2FA_qr_code"), {
-							text : url_qr_code_values,
-							width : 200,
-							height : 200,
-							colorDark : "#000000",
-							colorLight : "#ffffff",
-							correctLevel : QRCode.CorrectLevel.L
-						// like charts.googleapis.com
+					// ajax
+					$('#2FA_check_code').click(function() {
+						url = "./?_action=plugin.mel_doubleauth-checkcode&code="
+								+ $('#2FA_code_to_check').val();
+						$.post(url, function(data) {
+							alert(data);
+							if (data == rcmail.gettext('code_ok', 'mel_doubleauth')) {
+								$('.2FA_save').removeAttr('disabled');
+							}
 						});
-						url_qr_code_values = 'otpauth://totp/image_qr_code&issuer=M2Web';
-						var qrcode = new QRCode(document
-								.getElementById("img_qr_code"), {
-							text : url_qr_code_values,
-							width : 20,
-							height : 20,
-							colorDark : "#5882FA",
-							colorLight : "#ffffff",
-							correctLevel : QRCode.CorrectLevel.L
-						// like charts.googleapis.com
-						});
-	
-						$('#2FA_qr_code').prop('title', ''); // enjoy the
-																									// silence
-																									// (qrcode.js uses
-																									// text to set
-																									// title)
-	
 					});
 
-				function createCode() {
-					var min = 0;
-					var max = 9;
-					var n = 0;
-					var x = '';
+					// add qr-code before msg_info
+					let url_qr_code_values = 'otpauth://totp/'
+						+ document.querySelector('#_username').value
+						+ '?secret=' + secret
+						+ '&issuer=M2Web';
 
-					while (n < 6) {
-						n++;
-						x += Math.floor(Math.random() * (max - min)) + min;
-					}
-					return x;
-				}
+					let qrcode = new QRCode(document.getElementById("2FA_qr_code"), {
+						text : url_qr_code_values,
+						width : 200,
+						height : 200,
+						colorDark : "#000000",
+						colorLight : "#ffffff",
+						correctLevel : QRCode.CorrectLevel.L
+					});
+
+					url_qr_code_values = 'otpauth://totp/image_qr_code&issuer=M2Web';
+					qrcode = new QRCode(document.getElementById("img_qr_code"), {
+						text : url_qr_code_values,
+						width : 20,
+						height : 20,
+						colorDark : "#5882FA",
+						colorLight : "#ffffff",
+						correctLevel : QRCode.CorrectLevel.L
+					});
+
+					$('#2FA_qr_code').prop('title', ''); 	// enjoy the
+															// silence
+															// (qrcode.js uses
+															// text to set
+															// title)
+
+				});
+
+				
 			}
 
 			// populate all fields
 			function setup2FAfields() {
+				function createCode() {
+					let min = 0; let max = 9;
+					let n = 0; let x = '';
+
+					while (n < 6) {
+						n++; x += Math.floor(Math.random() * (max - min)) + min;
+					}
+					return x;
+				}
+
 				rcmail.http_request('plugin.mel_doubleauth-adduser');
 
 				$('#mel_doubleauth-form :input').each(function() {
@@ -137,9 +166,9 @@ if (window.rcmail) {
 
 			}
 
-			$('#2FA_activate_button').click(function() {
+			$('#p2FA_activate_button').click(function() {
 				setup2FAfields();
-				$('#2FA_activate_button').attr('disabled', 'disabled');
+				$('#p2FA_activate_button').attr('disabled', 'disabled');
 				window.double_fact_saved = false;
 			});
 
@@ -196,7 +225,7 @@ if (window.rcmail) {
 			});
 
 			$('#2FA_desactivate_button').click(function() {
-				$('#2FA_secret').get(0).value = '';
+				$('#p2FA_secret').get(0).value = '';
 				$("[name^='2FA_recovery_codes']").each(function() {
 					$(this).get(0).value = '';
 				});

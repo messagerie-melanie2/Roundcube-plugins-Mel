427f0f74931c6238870b3a52b5c7e6df26630c29
Rotomeca <delphin.tommy@gmail.com>
2022-03-25 16:23:37 +0100

    0006622: Rappel d'agenda, problème lorsqu'on veut décaler une deuxième fois le rappel

---
+++ plugins/mel_plugin_ext/mel_plugin_ext.php
@@ -0,0 +1,20 @@
+<?php
+class mel_plugin_ext extends rcube_plugin
+{
+    public $task = '.*';
+
+    public const PLUGINS = ['libcalendaring'];
+
+    public function init()
+    {
+        $this->add_hook('ready', array($this, 'ready'));
+    }
+
+    public function ready($args)
+    {
+        foreach (self::PLUGINS as $value) {
+            $this->include_script("js/$value/ext.js");
+        }
+        return $args;
+    }
+}

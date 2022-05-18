8ad8eb9fb5689249158086040d68371cb3d308f1
Rotomeca <delphin.tommy@gmail.com>
2022-05-18 10:21:56 +0200

    Affichage des évènements en passant par html.Calendar

--- plugins/mel_portal/modules/my_day/js/my_day.js
+++ plugins/mel_portal/modules/my_day/js/my_day.js
@@ -58,165 +58,177 @@ function my_day_generate_link(event)
  */
 function setupMyDay(datas)
 {
-	const classes = {
-		organizer:"icofont-royal royal",
-		tick:"icofont-check lightgreen",
-		waiting:"icofont-hour-glass clear",
-		declined:"icofont-close danger"
-	}
-	const set_style = (event) => {
-		const now = {
-			now:moment(),
-			start:moment().startOf('day'),
-			end:moment().endOf('day')
-		}
-		const date = {
-			start:moment(event.start),
-			end:moment(event.end)
-		}
-		if (date.start < now.start || date.end > now.end)
-			return {
-				start:date.start.format("DD/MM/YYYY HH:mm"),
-				end:date.end.format("DD/MM/YYYY HH:mm"),
-			}
-		else
-			return {
-				start:date.start.format("HH:mm"),
-				end:date.end.format("HH:mm"),
-			}
-	};
-	let html = ''
-	let style;
-	let link;
-	let bool = false;
-	let icon;
-	let title;
+	html_helper.Calendars({
+		datas,
+		config:{
+			add_day_navigation:false,
+			add_create:false,
+			create_function:null,
+			add_see_all:false,
+			next_when_empty_today_function:null
+		},
+		e:$("#agenda"),
+		e_number:$("#agendanew"),
+		get_only_body:true
+	});
 
-	if (datas.length > 0)
-	{
-		for (let index = 0; index < datas.length; index++) {
-			const element = datas[index];
+	// const classes = {
+	// 	organizer:"icofont-royal royal",
+	// 	tick:"icofont-check lightgreen",
+	// 	waiting:"icofont-hour-glass clear",
+	// 	declined:"icofont-close danger"
+	// }
+	// const set_style = (event) => {
+	// 	const now = {
+	// 		now:moment(),
+	// 		start:moment().startOf('day'),
+	// 		end:moment().endOf('day')
+	// 	}
+	// 	const date = {
+	// 		start:moment(event.start),
+	// 		end:moment(event.end)
+	// 	}
+	// 	if (date.start < now.start || date.end > now.end)
+	// 		return {
+	// 			start:date.start.format("DD/MM/YYYY HH:mm"),
+	// 			end:date.end.format("DD/MM/YYYY HH:mm"),
+	// 		}
+	// 	else
+	// 		return {
+	// 			start:date.start.format("HH:mm"),
+	// 			end:date.end.format("HH:mm"),
+	// 		}
+	// };
+	// let html = ''
+	// let style;
+	// let link;
+	// let bool = false;
+	// let icon;
+	// let title;
 
-			if (element.status === "CANCELLED")
-				continue;
+	// if (datas.length > 0)
+	// {
+	// 	for (let index = 0; index < datas.length; index++) {
+	// 		const element = datas[index];
 
-			title = mel_metapage.Functions.updateRichText(element.title);
+	// 		if (element.status === "CANCELLED")
+	// 			continue;
 
-			if (element.free_busy === "free")
-				title += " (libre)";
+	// 		title = mel_metapage.Functions.updateRichText(element.title);
 
-			if (element.attendees !== undefined && element.attendees.length > 0)
-			{
-				bool = false;
-				const item = Enumerable.from(element.attendees).where(x => x.email === rcmail.env.mel_metapage_user_emails[0]).firstOrDefault(null);
-				if (item !== null)
-				{
-					try {
-						switch (item.status) {
-							case "NEEDS-ACTION":
-								title += ` (En attente)`;
-								break;
+	// 		if (element.free_busy === "free")
+	// 			title += " (libre)";
+
+	// 		if (element.attendees !== undefined && element.attendees.length > 0)
+	// 		{
+	// 			bool = false;
+	// 			const item = Enumerable.from(element.attendees).where(x => x.email === rcmail.env.mel_metapage_user_emails[0]).firstOrDefault(null);
+	// 			if (item !== null)
+	// 			{
+	// 				try {
+	// 					switch (item.status) {
+	// 						case "NEEDS-ACTION":
+	// 							title += ` (En attente)`;
+	// 							break;
 	
-							case "ACCEPTED":
-								title += ` (Accepté)`;
-								break;
+	// 						case "ACCEPTED":
+	// 							title += ` (Accepté)`;
+	// 							break;
 	
-							case "TENTATIVE":
-								title += ` (Peut-être)`;
-								break;
+	// 						case "TENTATIVE":
+	// 							title += ` (Peut-être)`;
+	// 							break;
 	
-							case "CANCELLED":
-								bool = true;
-								break;
+	// 						case "CANCELLED":
+	// 							bool = true;
+	// 							break;
 						
-							default:
-								break;
-						}
-					} catch (error) {
+	// 						default:
+	// 							break;
+	// 					}
+	// 				} catch (error) {
 						
-					}
-				}
+	// 				}
+	// 			}
 
-				if (bool)
-					continue;
+	// 			if (bool)
+	// 				continue;
 
-			}
+	// 		}
 
-			html += "<li>";
-			html += "<div class=row style=margin-bottom:15px;margin-right:15px; >";
+	// 		html += "<li>";
+	// 		html += "<div class=row style=margin-bottom:15px;margin-right:15px; >";
 
-			if (element.allDay)
-				html += `<div class=col-8><a href=# class="element-block mel-not-link mel-focus" onclick="${my_day_generate_link(element)}"><span class="element-title default-text bold element-block">` + rcmail.gettext("Journée entière") + `</span><span class="element-desc secondary-text element-block">` + title + `</span></a></div>`;
-			else
-			{
-				const style_date = set_style(element);
-				html += `<div class=col-8><a href=# class="element-block mel-not-link mel-focus" onclick="${my_day_generate_link(element)}"><span class="element-title default-text bold element-block">` + style_date.start + " - " + style_date.end + `</span><span class="element-desc secondary-text element-block">` + title +"</span></a></div>";
-			}
+	// 		if (element.allDay)
+	// 			html += `<div class=col-8><a href=# class="element-block mel-not-link mel-focus" onclick="${my_day_generate_link(element)}"><span class="element-title default-text bold element-block">` + rcmail.gettext("Journée entière") + `</span><span class="element-desc secondary-text element-block">` + title + `</span></a></div>`;
+	// 		else
+	// 		{
+	// 			const style_date = set_style(element);
+	// 			html += `<div class=col-8><a href=# class="element-block mel-not-link mel-focus" onclick="${my_day_generate_link(element)}"><span class="element-title default-text bold element-block">` + style_date.start + " - " + style_date.end + `</span><span class="element-desc secondary-text element-block">` + title +"</span></a></div>";
+	// 		}
 
-			if (rcube_calendar.is_desc_webconf(element.location))//element.location.includes("@visio") || element.location.includes("#visio") || element.location.includes(rcmail.env["webconf.base_url"]))
-			{
-				style = "";
-				if (element.location.includes("@visio"))
-					link = `target="_blank" href="${element.location.replace("@visio:", "")}"`;
-				else {
-					var tmp_link = WebconfLink.create(element);
-					link = `href="#" onclick="window.webconf_helper.go('${tmp_link.key}', ${tmp_link.get_wsp_string()}, ${tmp_link.get_ariane_string()})"`;	
-				}
-				// else if (element.location.includes("#visio"))
-				// {
-				// 	var tmp_link = new WebconfLink(element.location);
-				// 	link = `href="#" onclick="window.webconf_helper.go('${tmp_link.key}', ${tmp_link.get_wsp_string()}, ${tmp_link.get_ariane_string()})"`;
-				// }
-				// else
-				// {
-				// 	const categoryExist = element.categories !== undefined && element.categories !== null && element.categories.length > 0;
-				// 	const isWsp = categoryExist && element.categories[0].includes("ws#");
-				// 	const ariane = isWsp ? "null" : "'@home'";
-				// 	const wsp = isWsp ? `'${element.categories[0].replace("ws#", "")}'` : "null";
-				// 	link = `href="#" onclick="window.webconf_helper.go('${mel_metapage.Functions.webconf_url(element.location)}', ${wsp}, ${ariane})"`;
-				// }
-			}
-			else
-				style = "display:none;";
+	// 		if (rcube_calendar.is_desc_webconf(element.location))//element.location.includes("@visio") || element.location.includes("#visio") || element.location.includes(rcmail.env["webconf.base_url"]))
+	// 		{
+	// 			style = "";
+	// 			if (element.location.includes("@visio"))
+	// 				link = `target="_blank" href="${element.location.replace("@visio:", "")}"`;
+	// 			else {
+	// 				var tmp_link = WebconfLink.create(element);
+	// 				link = `href="#" onclick="window.webconf_helper.go('${tmp_link.key}', ${tmp_link.get_wsp_string()}, ${tmp_link.get_ariane_string()})"`;	
+	// 			}
+	// 			// else if (element.location.includes("#visio"))
+	// 			// {
+	// 			// 	var tmp_link = new WebconfLink(element.location);
+	// 			// 	link = `href="#" onclick="window.webconf_helper.go('${tmp_link.key}', ${tmp_link.get_wsp_string()}, ${tmp_link.get_ariane_string()})"`;
+	// 			// }
+	// 			// else
+	// 			// {
+	// 			// 	const categoryExist = element.categories !== undefined && element.categories !== null && element.categories.length > 0;
+	// 			// 	const isWsp = categoryExist && element.categories[0].includes("ws#");
+	// 			// 	const ariane = isWsp ? "null" : "'@home'";
+	// 			// 	const wsp = isWsp ? `'${element.categories[0].replace("ws#", "")}'` : "null";
+	// 			// 	link = `href="#" onclick="window.webconf_helper.go('${mel_metapage.Functions.webconf_url(element.location)}', ${wsp}, ${ariane})"`;
+	// 			// }
+	// 		}
+	// 		else
+	// 			style = "display:none;";
 
-			html += '<div class=col-4><div class="webconf-myday"><a '+link+' style="'+style+'" class="roundbadge link large dark icon-mel-videoconference"><span class="sr-only">Aller à la Webconf</span></a><span style="'+style+'" class="span-webconf">Webconf</span></div></div>';
-			html += "</div>";
-			html += "</li>";
-		}
-	}
-	else 
-	{
-		const storage = Enumerable.from(mel_metapage.Storage.get(mel_metapage.Storage.calendar_by_days));
-		const storage_count = storage.count();
-		if (storage_count > 0)
-		{
-			const storage_first = storage.first();
-			const value = storage_first.value[0];
-			const all_day = value.allDay ? "_all_day" : "";
-			html += `<li><span class="element-title element-no default-text bold element-block">${rcmail.gettext('mel_portal.no_event_today')}</span>
-			<a href=# class="element-block mel-not-link mel-focus" onclick="${my_day_generate_link(value)}">
-			<span class="element-title default-text bold element-block">${rcmail.gettext(`mel_portal.next_agenda_event${all_day}`).replace('{date}', storage_first.key).replace('{horaire}', moment(value.start).format('HH:mm'))}</span>
-			<span class="element-desc secondary-text element-block">${value.title}</span>
-			</a>
-			</li>`;
-		}
-		else html += `<li>Pas d'évènements aujourd'hui ainsi que dans les 7 prochains jours !</li>`;
-	}
+	// 		html += '<div class=col-4><div class="webconf-myday"><a '+link+' style="'+style+'" class="roundbadge link large dark icon-mel-videoconference"><span class="sr-only">Aller à la Webconf</span></a><span style="'+style+'" class="span-webconf">Webconf</span></div></div>';
+	// 		html += "</div>";
+	// 		html += "</li>";
+	// 	}
+	// }
+	// else 
+	// {
+	// 	const storage = Enumerable.from(mel_metapage.Storage.get(mel_metapage.Storage.calendar_by_days));
+	// 	const storage_count = storage.count();
+	// 	if (storage_count > 0)
+	// 	{
+	// 		const storage_first = storage.first();
+	// 		const value = storage_first.value[0];
+	// 		const all_day = value.allDay ? "_all_day" : "";
+	// 		html += `<li><span class="element-title element-no default-text bold element-block">${rcmail.gettext('mel_portal.no_event_today')}</span>
+	// 		<a href=# class="element-block mel-not-link mel-focus" onclick="${my_day_generate_link(value)}">
+	// 		<span class="element-title default-text bold element-block">${rcmail.gettext(`mel_portal.next_agenda_event${all_day}`).replace('{date}', storage_first.key).replace('{horaire}', moment(value.start).format('HH:mm'))}</span>
+	// 		<span class="element-desc secondary-text element-block">${value.title}</span>
+	// 		</a>
+	// 		</li>`;
+	// 	}
+	// 	else html += `<li>Pas d'évènements aujourd'hui ainsi que dans les 7 prochains jours !</li>`;
+	// }
 
-    html = `<ul class="ignore-bullet">${html}</ul>`;
-	$("#agenda").html(html);
+    // html = `<ul class="ignore-bullet">${html}</ul>`;
+	// $("#agenda").html(html);
 
-	const count = Enumerable.from(datas).where(x => x.free_busy !== 'free').count();
+	// const count = Enumerable.from(datas).where(x => x.free_busy !== 'free').count();
 
-	if (count > 0)
-	{
-		$("#agendanew").html(count);
-		$("#agendanew").removeClass("hidden");
-	}
-	else
-		$("#agendanew").addClass("hidden");
-
-		
+	// if (count > 0)
+	// {
+	// 	$("#agendanew").html(count);
+	// 	$("#agendanew").removeClass("hidden");
+	// }
+	// else
+	// 	$("#agendanew").addClass("hidden");
 }
 
 /**

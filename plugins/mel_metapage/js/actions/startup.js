e7e7532b02db220c52b23dabb37c8f5b177fc8ec
Rotomeca <delphin.tommy@gmail.com>
2022-06-10 17:14:23 +0200

    Corrections visio

--- plugins/mel_metapage/js/actions/startup.js
+++ plugins/mel_metapage/js/actions/startup.js
@@ -374,7 +374,8 @@ metapage_frames.addEvent("before", (eClass, changepage) => {
             }
 
             const icon = Enumerable.from($(querry_selector)[0]?.classList ?? []).firstOrDefault(x => x.includes('icon-mel'), '');
-            mel_metapage.Frames.add(mel_metapage.Frames.create_frame(rcmail.env.last_frame_name, rcmail.env.last_frame_class, icon));
+            
+            if (rcmail.env.last_frame_class !== undefined) mel_metapage.Frames.add(mel_metapage.Frames.create_frame(rcmail.env.last_frame_name, rcmail.env.last_frame_class, icon));
         }
     }
 });

8d5a9131eff9dde1652c62ac34f4cdd657bee68c
Rotomeca <delphin.tommy@gmail.com>
2022-08-30 09:36:58 +0200

    Ajout des constants manquants + ajout de la tâche 'stockage'

--- plugins/mel_metapage/program/consts/consts.php
+++ plugins/mel_metapage/program/consts/consts.php
@@ -2,7 +2,13 @@
 abstract class Consts 
 {
     public const TASK_CHAT = 'chat';
+    public const TASK_DRIVE = 'stockage';
+
+    public const FUNCTION_INDEX = 'page_index';
+
     public const HOOK_NAMESPACE_SPERATOR = '.';
     public const HOOK_HAVE_PLUGIN = 'have_plugin';
+    public const HOOK_REGISTER_MODULE = 'register_module';
+    
     public const RETURN = 'return_value';
 }

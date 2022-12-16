1f54c6538642755ca9c3ed1f3eada1565fa89526
AGoubs <arnaud@goubier.fr>
2022-12-16 10:15:55 +0100

    Aide toujours visible même avec #

--- plugins/mel_onboarding/onboarding.js
+++ plugins/mel_onboarding/onboarding.js
@@ -39,7 +39,6 @@ if (window.rcmail) {
 
 //Ouvre l'onboarding depuis la fenêtre d'assistance
 rcube_webmail.prototype.current_page_onboarding = function (task) {
-  // window.parent.help_popUp.close();
   window.help_popUp.close();
   var iframe = window.parent.$('iframe.' + task + '-frame')[0];
   if (iframe) {

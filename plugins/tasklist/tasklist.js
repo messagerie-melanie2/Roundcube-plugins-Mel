d8be0e8d587ad57fa7268260ab1658bd8a869930
Rotomeca <tommy.delphin@i-carre.net>
2023-03-08 10:00:40 +0100

    fix(tasks): Les tâches ne fonctionnaient plus

--- plugins/tasklist/tasklist.js
+++ plugins/tasklist/tasklist.js
@@ -300,7 +300,9 @@ function rcube_tasklist_ui(settings)
         rcmail.addEventListener('plugin.update_tasklist', update_list);
         rcmail.addEventListener('plugin.destroy_tasklist', destroy_list);
         rcmail.addEventListener('plugin.unlock_saving', unlock_saving);
-        rcmail.addEventListener('plugin.refresh_tagcloud', function() { update_tagcloud(); });
+        rcmail.addEventListener('plugin.refresh_tagcloud', function() { 
+            if ("undefined" !== typeof update_tagcloud) update_tagcloud(); 
+        });
         rcmail.addEventListener('requestrefresh', before_refresh);
         rcmail.addEventListener('plugin.reload_data', function(){
             list_tasks(null, true);

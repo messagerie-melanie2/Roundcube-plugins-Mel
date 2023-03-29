fc357f0a0d4b28e3ecc7ff5b79a4bdc9ea939aa0
Rotomeca <tommy.delphin@i-carre.net>
2023-03-29 09:30:06 +0200

    fix(stockage): Changement de la façon dont fonctionne why_stockage_not_active()

--- plugins/mel_helper/mel_helper.php
+++ plugins/mel_helper/mel_helper.php
@@ -124,14 +124,13 @@ class mel_helper extends rcube_plugin
 
     public static function why_stockage_not_active()
     {
-        if (!self::stockage_active())
-            return self::ST_NO_RIGHTS;
-        else if (!mel::is_internal() 
+        if (self::stockage_active()) self::ST_ACTIVE;
+        else if (!mel::is_auth_strong()
         && class_exists('mel_doubleauth')
         && !mel_doubleauth::is_double_auth_enable())
             return self::ST_NO_DOUBLE_AUTH;
 
-        return self::ST_ACTIVE;
+        return self::ST_NO_RIGHTS;
     }
 
     public static function color()

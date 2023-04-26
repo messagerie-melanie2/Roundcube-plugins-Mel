097cfe6f1a394e7b9c659b8612324d8438b09c49
Rotomeca <tommy.delphin@i-carre.net>
2023-04-26 11:37:11 +0200

    refactor(mel_portal): Suppression des modules inutilisés

--- plugins/mel_portal/modules/flux_rss/get_flux_intra.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-## Intranet
-sudo wget http://intra.ac.e2.rie.gouv.fr/spip.php?page=backend-actu -O rss/intranet-mtes-ac.xml
-sudo wget http://intra.snum.sg.e2.rie.gouv.fr/spip.php?page=backend-actu -O rss/intranet-mtes-snum.xml
-sudo wget http://intra.dgitm.i2/spip.php?page=backend-actu -O rss/intranet-dgitm.xml
-sudo wget http://intra.portail.e2.rie.gouv.fr/spip.php?page=backend-actu -O rss/intranet-mtes.xml
-##extranet
--- plugins/mel_portal/modules/left_menu/conf.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-include_once "menu_item.php";
-$config["dashboard"] = new MenuItem("index", "index");
-$config["news"] = new MenuItem("news", "news");
-$config["config"] = new MenuItem("config", "config");

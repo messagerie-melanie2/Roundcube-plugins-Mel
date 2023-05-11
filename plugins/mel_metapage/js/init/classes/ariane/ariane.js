da2376d15af5951f689051e1af6e91e20a83ac69
Rotomeca <tommy.delphin@i-carre.net>
2023-05-11 10:29:20 +0200

    feat(chat): add OpenCustomStatusModal

--- plugins/mel_metapage/js/init/classes/ariane/ariane.js
+++ plugins/mel_metapage/js/init/classes/ariane/ariane.js
@@ -18,6 +18,40 @@ class ChatHelper {
     static async Top() {
         return (await loadJsModule('mel_metapage', 'Top')).Top;
     }
+
+    static OpenCustomStatusModal() {
+        let html = '<div class="d-flex">'+
+          '<input type="text" id="custom_status_input" placeholder="Que faites-vous en ce moment ?">'+
+          '<select id="custom_status_dropdown">' +
+          '<option value="online">Disponible</option>' +
+          '<option value="away">Absent</option>' +
+          '<option value="busy">Ne pas déranger</option>' +
+          '<option value="offline">Hors ligne</option>' +
+          '</select>'+
+      '</div>';
+
+        let buttons = [
+        {
+          text: 'Annuler',
+          click: function () {
+            $(this).dialog('destroy');
+          }
+        },
+        {
+          text: 'Enregistrer',
+          class: 'btn btn-primary border-0 text-light',
+          click: function () {
+            var message = document.getElementById('custom_status_input').value;
+            var status = document.getElementById('custom_status_dropdown').value;
+            ariane.set_update_status(status, message)
+            $(this).dialog('destroy');
+          }
+        }]
+
+        // Display the popup dialog
+        rcmail.show_popup_dialog(html, rcmail.gettext('mel_metapage.change_status'), buttons, {resizable: false, draggable: false});
+
+      }
 }
 
 

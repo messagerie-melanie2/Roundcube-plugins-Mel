3f841c2e747a583f0fdd308d6581a4c9ea0f4c1b
Rotomeca <tommy.delphin@i-carre.net>
2023-06-07 17:18:44 +0200

    feat(notes): Passage des notes en framework bnum

---
+++ plugins/mel_metapage/js/lib/metapages_actions/notes.js
@@ -0,0 +1,184 @@
+import { MelFullScreenItem } from "../classes/fullscreen";
+import { MetapageModule } from "./metapage_module";
+import { Sticker, default_note_uid } from "./notes/sticker";
+
+export class MetapageNotesModule extends MetapageModule {
+    constructor() {
+        super();
+    }
+
+    main() {
+        super.main();
+
+        this._fullscreen = null;
+        this.state = false;
+
+        this.notes = null;
+        Object.defineProperties(this, {
+            notes: {
+                get: () => {
+                    let notes;
+                    const raw_notes = this.get_env('mel_metapages_notes');
+
+                    if (!Enumerable.from(raw_notes ?? []).any())
+                    {
+                        notes = {};
+                        notes[default_note_uid] = new Sticker(default_note_uid, 0, "", "");
+                    }
+                    else notes = raw_notes;
+
+                    return notes;
+                },
+                configurable: true
+            },          
+        });
+
+        this.select_note_button().click(() => {
+            if (!this._fullscreen) {
+                this.add_event_listener('notes.apps.updated', () => {
+                    this._generate_notes();
+                }, {callback_key:'notes_modules'});
+
+                this._create_fullscreen_item()
+                ._generate_notes()
+                .show();
+            }
+            else this.toggle();
+        });
+    }
+
+    select_note_button() {
+        return this.select('#button-notes');
+    }
+
+    _create_fullscreen_item() {
+        this._fullscreen = new MelFullScreenItem('app-notes', 'body', {close_on_click:false});
+
+        this._fullscreen.onclose.push(() => {
+            this.state = false;
+        });
+
+        this._fullscreen.onshow.push(() => {
+            this.state = true;
+        });
+
+        this._fullscreen.$apps[0].addEventListener('dragover', (ev) => {
+            ev.preventDefault();
+            ev.dataTransfer.dropEffect = "move";
+
+            if (!$(ev.target).hasClass('fullscreen_container')) {
+                let $parent = $(ev.target);
+
+                while(!$parent.hasClass('mel-note') && $parent[0].nodeName !== 'BODY') {
+                    $parent = $parent.parent();
+                }
+
+                if ($parent.hasClass('mel-note')) {
+
+                    if (!$parent.hasClass('dragover')) {
+                        $('.mel-note.dragover').removeClass('dragover');
+                        $('.mel-note.dragover-right').removeClass('dragover-right');
+                        $('.mel-note.dragover-left').removeClass('dragover-left');
+                        $parent.addClass('dragover');
+                    }
+
+                    const rect = $parent[0].getBoundingClientRect();
+                    const x = ev.clientX - rect.left;
+                    const y = ev.clientY - rect.left;
+
+                    //console.log('dragover', rect, x, y, ($parent.width() / 2));
+                    if (x >= ($parent.width() / 2)) $parent.addClass('dragover-right').removeClass('dragover-left');
+                    else $parent.addClass('dragover-left').removeClass('dragover-right');
+                }
+            }
+            else {
+                $('.mel-note.dragover').removeClass('dragover');
+                $('.mel-note.dragover-right').removeClass('dragover-right');
+                $('.mel-note.dragover-left').removeClass('dragover-left');
+            }
+        });
+
+        this._fullscreen.$apps[0].addEventListener('drop', (ev) => {
+            ev.preventDefault();
+            console.log('drop', ev);
+            const uid = ev.dataTransfer.getData("text/plain");
+
+            let $target = $('.mel-note.dragover');
+            if ($target.length > 0) {
+                let sticker_target = Sticker.fromHtml($target.attr('id').replace('note-', EMPTY_STRING));
+
+                if (sticker_target.uid !== uid) {
+                    let order = +sticker_target.order;
+
+                    if ($target.hasClass('dragover-right')) order += 1;
+                    else order -= 1;
+    
+                    Sticker.fromHtml(uid).drop(uid, order);
+                }
+            }
+
+            $('.mel-note.dragover').removeClass('dragover');
+            $('.mel-note.dragover-right').removeClass('dragover-right');
+            $('.mel-note.dragover-left').removeClass('dragover-left');
+            // var data = ev.dataTransfer.getData("text/html");
+            // ev.target.appendChild(document.getElementById(data));
+        });
+
+        return this;
+    }
+
+    _generate_notes() {
+        let focused_sticker = this.in_focus_mode() ? this.get_note_focused() : null;
+
+        this._fullscreen.clear();
+
+        let stickers = [];
+        let current_sticker;
+        for (const iterator of Enumerable.from(this.notes).orderBy(x => x.value.order)) {
+            const {key, value:note} = iterator;
+            current_sticker = Sticker.from(note);
+            this._fullscreen.add(key, $(current_sticker.html()));
+            stickers.push(current_sticker);
+            current_sticker = null;
+        }
+
+        for (let index = 0, len = stickers.length; index < len; ++index) {
+            stickers[index].set_handlers();
+        }
+
+        if (!!focused_sticker) focused_sticker.get_html().find('button.eye').click();
+
+        return this;
+    }
+
+    in_focus_mode() {
+        return this._fullscreen.$element.find('.mel-note .eye.crossed').length > 0;
+    }
+
+    get_note_focused() {
+        let $parent = this._fullscreen.$element.find('.mel-note .eye.crossed');
+
+        do {
+            $parent = $parent.parent();
+        } while (!$parent.hasClass('mel-note'));
+
+        return Sticker.fromHtml($parent.attr('id').replace('note-', EMPTY_STRING));
+    }
+
+    hide() {
+        this._fullscreen.hide();
+        return this;
+    }
+
+    show() {
+        this._fullscreen.show();
+        return this;
+    }
+
+    toggle() {
+        if (this.state)  this.hide();
+        else this.show();
+
+        return this;
+    }
+}

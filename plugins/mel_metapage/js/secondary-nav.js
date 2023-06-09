53c5ed462ff837eaf4140351a3daefd6093a3b8b
AGoubs <arnaud@goubier.fr>
2023-06-09 10:54:09 +0200

    fix(barup) : Correction responsive pour la barre de recherche globale

--- plugins/mel_metapage/js/secondary-nav.js
+++ plugins/mel_metapage/js/secondary-nav.js
@@ -1,12 +1,16 @@
 function open_research() {
-  $('#barup-search-col').toggle()
+  $('#barup-search-col').toggle();
+  $('.hide-button-search').toggle();
 }
 
 
 $(window).resize(function () {
-  if ($('#barup-search-col').is(":hidden")) {
-    if ($(this).width() > 480) {
-      $('#barup-search-col').show()
+  if ($(this).width() > 480) {
+    if ($('#barup-search-col').is(":hidden")) {
+      $('#barup-search-col').show();
+    }
+    else {
+      $('.hide-button-search').show();
     }
   }
 });

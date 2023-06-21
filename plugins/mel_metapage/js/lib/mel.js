87aab5f0c506c96f70fa7e788c04d418a9bad22d
Rotomeca <tommy.delphin@i-carre.net>
2023-06-21 12:03:19 +0200

    adding `Classes`

--- plugins/mel_metapage/js/lib/mel.js
+++ plugins/mel_metapage/js/lib/mel.js
@@ -1,4 +1,4 @@
-export {isNullOrUndefined, isArrayLike};
+export {isNullOrUndefined, isArrayLike, Classes};
 
 /**
  * Renvoie vrai si la variable vaut `null` ou `undefined`.
@@ -26,4 +26,28 @@ function isArrayLike(item) {
           (item.length - 1) in item
         )
     );
+}
+
+var Classes = (baseClass, ...mixins) => {
+  class base extends baseClass {
+      constructor (...args) {
+          super(...args);
+          mixins.forEach((mixin) => {
+              copyProps(this,(new mixin));
+          });
+      }
+  }
+  let copyProps = (target, source) => {  // this function copies all properties and symbols, filtering out some special ones
+      Object.getOwnPropertyNames(source)
+            .concat(Object.getOwnPropertySymbols(source))
+            .forEach((prop) => {
+               if (!prop.match(/^(?:constructor|prototype|arguments|caller|name|bind|call|apply|toString|length)$/))
+                  Object.defineProperty(target, prop, Object.getOwnPropertyDescriptor(source, prop));
+             })
+  }
+  mixins.forEach((mixin) => { // outside contructor() to allow aggregation(A,B,C).staticFunction() to be called etc.
+      copyProps(base.prototype, mixin.prototype);
+      copyProps(base, mixin);
+  });
+  return base;
 }

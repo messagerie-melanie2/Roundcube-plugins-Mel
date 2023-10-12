b05ed3d5aa4f0da665cb5fb6a4ed03c4f27bd2de
Rotomeca <tommy.delphin@i-carre.net>
2023-10-12 09:17:47 +0200

    Updating test

--- plugins/mel_metapage/js/actions/test.js
+++ plugins/mel_metapage/js/actions/test.js
@@ -1,40 +1,10 @@
-async function get_status() {
-    let status_datas = {
-        status:undefined,
-        message:''
-    }
-    await mel_metapage.Functions.get(
-        mel_metapage.Functions.url('discussion', 'get_status'),
-        {},
-        (datas) => {
-            if ("string" === typeof datas) datas = JSON.parse(datas);
-            console.log('datas', datas);
-            status_datas.status = datas.content.status;
-            status_datas.message = datas.content.message || '';
-        }
-    );
+$(document).ready(async () => {
+    // debugger;
+    const BnumConnector = await module_helper_mel.BnumConnector();
 
-    return status_datas;
-}
-
-async function set_status(status, message) {
-    await mel_metapage.Functions.post(
-        mel_metapage.Functions.url('discussion', 'set_status'),
-        {
-            _st:status,
-            _msg:message
-        },
-        (datas) => {
-            if ("string" === typeof datas) datas = JSON.parse(datas);
-
-            console.log("test", datas);
-        }
-    );
-}
-$(document).ready(() => {
-
-    console.log('start', rcmail.env.task);
-    if (rcmail.env.task === 'rotomecatest') {
-        get_status();
-    }
+    let config = BnumConnector.connectors.settings_da_set_email_recup.needed;
+    config._val = 'delphin.tommy@gmail.com';
+    await BnumConnector.connect(BnumConnector.connectors.settings_da_set_email_recup, {
+        params:config,
+    });
 });

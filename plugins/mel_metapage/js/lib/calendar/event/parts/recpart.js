005f6d3ed690227de3c7ec97ba6b5797911342ca
Rotomeca <tommy.delphin@i-carre.net>
2024-03-06 11:03:34 +0100

    fix(event): Correctionde fautes + correction css

--- plugins/mel_metapage/js/lib/calendar/event/parts/recpart.js
+++ plugins/mel_metapage/js/lib/calendar/event/parts/recpart.js
@@ -1,8 +1,5 @@
 import { FakePart, Parts } from "./parts.js";
 
-/** $("#edit-recurrence-frequency").val(e.target.value);
-  $("#edit-recurrence-frequency").change(); */
-
 export class RecPart extends FakePart {
     constructor(original, fake) {
         super(original, fake, Parts.MODE.change);

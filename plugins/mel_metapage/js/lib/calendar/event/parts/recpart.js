c428e236d9cfa6c21272fa4bb0d27af17ee6bb4b
DorianDuveau <dorian.duveau@apitech.fr>
2024-03-22 11:06:58 +0100

    feat(calendrier): Limiter le nombre d'occurence mantis 7956

--- plugins/mel_metapage/js/lib/calendar/event/parts/recpart.js
+++ plugins/mel_metapage/js/lib/calendar/event/parts/recpart.js
@@ -2,6 +2,7 @@
  * @module EventView/Parts/Reccursivity
  * 
  */
+import { EventView } from "../event_view.js";
 import { FakePart, Parts } from "./parts.js";
 
 /**
@@ -48,6 +49,42 @@ export class RecPart extends FakePart {
      */
     onChange(e) {
         this.onUpdate(e.currentTarget.value);
+        //récupère la première date
+        let firstdate = EventView.INSTANCE.parts.date.date_start;
+        //récupère la fréquence dayli, weekly, monthly, yearly
+        let freq = e.currentTarget.value
+        let endprop;
+        let end = true;
+        //calcule
+        switch (freq) {
+            case RecPart.frequency.daily:
+                endprop = firstdate.add(3, 'M');
+                //destrucs
+                break;
+            case RecPart.frequency.weekly:
+                //destrucs
+                endprop = firstdate.add(6, 'M');
+                break;
+            case RecPart.frequency.monthly:
+                //destrucs
+                endprop = firstdate.add(1, 'y');
+                break;
+            case RecPart.frequency.yearly:
+                end = false;
+                //destrucs
+                break;
+            default:
+                break;
+        }
+        if (end) {
+            //propose le champ
+            $('#edit-recurrence-enddate').val(endprop.format("DD/MM/YYYY"));
+            // met date cochée par défault
+            $('#edit-recurrence-repeat-until').prop('checked', true);
+        } else {
+            $('#edit-recurrence-enddate').val(null);
+            $('#edit-recurrence-repeat-forever').prop('checked', true);
+        }
     }
 
     /**
@@ -57,3 +94,7 @@ export class RecPart extends FakePart {
         this._$fakeField.tooltip('destroy');
     }
 }
+/**
+ * @enum {string}
+ */
+RecPart.frequency = {daily : 'DAILY', weekly : 'WEEKLY', monthly : 'MONTHLY', yearly : 'YEARLY'};

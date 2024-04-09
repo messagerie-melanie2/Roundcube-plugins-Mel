3e58c44323e6d8f9718ebc8f735483f791e081f4
AGoubs <arnaud@goubier.fr>
2024-04-09 11:47:46 +0200

    fix(notifications) : 0008219: Pouvoir passer une notification à lue

--- plugins/mel_notification/notifications.js
+++ plugins/mel_notification/notifications.js
@@ -901,14 +901,13 @@ class html_notification extends mel_html2 {
                 const action = notification.action[key];
                 this.attribs['href'] = action.href ?? '#',
                 this.onclick.push((e) => {
+                    m_mp_NotificationsAction('read', [btoa(notification.uid)]);
                     if (!!action.command) { 
                       rcmail.command(action.command, action.params ?? '', e);
-                      m_mp_NotificationsAction('read', [btoa(notification.uid)]);
                      }
                     else if (!!action.click) {
                       _action.click(e);
-                      m_mp_NotificationsAction('read', [btoa(notification.uid)]);}
-
+                    }
                     e.stopPropagation();
                 })
                 this.attribs['title'] = rcmail.get_label('mel_notification.Action title');

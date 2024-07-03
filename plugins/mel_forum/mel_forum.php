4090aff638545a7dfbe6f2781594fd8912ccac5b
CottonDamien <damien.cotton@hotmail.fr>
2024-07-03 17:34:55 +0200

    feat(forum): Creation du crud pour un article

--- plugins/mel_forum/mel_forum.php
+++ plugins/mel_forum/mel_forum.php
@@ -122,11 +122,11 @@ class mel_forum extends rcube_plugin
     function check_user()
     {
         $user;
-        $val = rcube_utils::get_input_value("_uid", rcube_utils::INPUT_GPC);
+        $val = rcube_utils::get_input_value('_uid', rcube_utils::INPUT_GPC);
         if (strpos($val, "@") !== false) $user = driver_mel::gi()->getUser(null, true, false, null, $val);
         else  $user = driver_mel::gi()->getUser($val);
 
-        echo $user === null ? "false" : json_encode(["uid" => $user->uid, "html" => $user->fullname]);
+        echo $user === null ? 'false' : json_encode(['uid' => $user->uid, 'html' => $user->fullname]);
 
         exit;
     }
@@ -140,40 +140,125 @@ class mel_forum extends rcube_plugin
         $workspace = driver_mel::gi()->get_workspace_group();
 
         // récupérer les valeurs des champs POST
-        $title = rcube_utils::get_input_value("title", rcube_utils::INPUT_POST);
-        $content = rcube_utils::get_input_value("content", rcube_utils::INPUT_POST);
-        $description = rcube_utils::get_input_value("description", rcube_utils::INPUT_POST);
-        $settings = rcube_utils::get_input_value("settings", rcube_utils::INPUT_POST);
+        $uid = rcube_utils::get_input_value('_uid', rcube_utils::INPUT_POST);
+        $title = rcube_utils::get_input_value('_title', rcube_utils::INPUT_POST);
+        $content = rcube_utils::get_input_value('_content', rcube_utils::INPUT_POST);
+        $description = rcube_utils::get_input_value('_description', rcube_utils::INPUT_POST);
+        $settings = rcube_utils::get_input_value('_settings', rcube_utils::INPUT_POST);
 
         // Validation des données saisies
-        if (empty($title) || empty($content) || empty($description) || empty($settings)) {
+        if (empty($uid) ||empty($title) || empty($content) || empty($description) || empty($settings)) {
             echo json_encode(['status' => 'error', 'message' => 'Tous les champs sont requis.']);
             exit;
         }
 
         //Créer un nouvel Article
-        $post = driver_mel::gi()->post();
+        $post = new LibMelanie\Api\Defaut\Posts\Post();
 
         //Définition des propriétés de l'article
+        $post->uid = $uid;
         $post->title = $title;
         $post->content = $content;
         $post->description = $description;
         $post->post_uid = uniqid();
         $post->created_at = date('Y-m-d H:i:s');
         $post->user_id = $user->uid;
-        $post->workspace_uid = $workspace->uid;
         $post->settings = $settings;
 
         // Sauvegarde de l'article
-        if ($post->save()) {
+        $ret = $post->save();
+        if (!is_null($ret)) {
             echo json_encode(['status' => 'success', 'message' => 'Article créé avec succès.']);
         } else {
-            echo json_encode(['status' => 'error', 'message' => 'Echec de création de l article.']);
+            echo json_encode(['status' => 'error', 'message' => 'Echec de création de l\'article.']);
         }
 
         // Arrêt de l'exécution du script
         exit;
     }
+
+    public function update_post()
+    {
+        // Récupérer l'utilisateur
+        $user = driver_mel::gi()->getUser();
+
+        // Récupérer les valeurs des champs POST
+        $uid = rcube_utils::get_input_value('_uid', rcube_utils::INPUT_POST);
+        $title = rcube_utils::get_input_value('_title', rcube_utils::INPUT_POST);
+        $content = rcube_utils::get_input_value('_content', rcube_utils::INPUT_POST);
+        $description = rcube_utils::get_input_value('_description', rcube_utils::INPUT_POST);
+        $settings = rcube_utils::get_input_value('_settings', rcube_utils::INPUT_POST);
+
+        // Validation des données saisies
+        if (empty($uid) || empty($title) || empty($content) || empty($description) || empty($settings)) {
+            echo json_encode(['status' => 'error', 'message' => 'Tous les champs sont requis.']);
+            exit;
+        }
+
+        // Récupérer l'article existant
+        $post = LibMelanie\Api\Defaut\Posts\Post($uid);
+
+        // Vérifier si l'article existe
+        if (!$post) {
+            echo json_encode(['status' => 'error', 'message' => 'Article introuvable.']);
+            exit;
+        }
+
+        // Définir les nouvelles propriétés de l'article
+        $post->title = $title;
+        $post->content = $content;
+        $post->description = $description;
+        $post->settings = $settings;
+        $post->updated_at = date('Y-m-d H:i:s');
+
+        // Sauvegarde de l'article
+        $ret = $post->save();
+        if (!is_null($ret)) {
+            echo json_encode(['status' => 'success', 'message' => 'Article mis à jour avec succès.']);
+        } else {
+            echo json_encode(['status' => 'error', 'message' => 'Echec de mise à jour de l\'article.']);
+        }
+
+        // Arrêt de l'exécution du script
+        exit;
+    }
+
+    public function delete_post()
+    {
+        // Récupérer l'utilisateur
+        $user = driver_mel::gi()->getUser();
+
+        // Récupérer la valeur du champ POST
+        $uid = rcube_utils::get_input_value('_uid', rcube_utils::INPUT_POST);
+
+        // Validation de la donnée saisie
+        if (empty($uid)) {
+            echo json_encode(['status' => 'error', 'message' => 'L\'identifiant de l\'article est requis.']);
+            exit;
+        }
+
+        // Récupérer l'article existant
+        $post = LibMelanie\Api\Defaut\Posts\Post($uid);
+
+        // Vérifier si l'article existe
+        if (!$post) {
+            echo json_encode(['status' => 'error', 'message' => 'Article introuvable.']);
+            exit;
+        }
+
+        // Supprimer l'article
+        $ret = $post->delete();
+        if ($ret) {
+            echo json_encode(['status' => 'success', 'message' => 'Article supprimé avec succès.']);
+        } else {
+            echo json_encode(['status' => 'error', 'message' => 'Echec de suppression de l\'article.']);
+        }
+
+        // Arrêt de l'exécution du script
+        exit;
+    }
+
+
 
     /**
      * Gestion de la frame

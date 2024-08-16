f30b18dbc6652c36c8757c015c2d9bd814b3ff4b
CottonDamien <damien.cotton@hotmail.fr>
2024-08-16 10:36:40 +0200

    feat(forum): Genération du html pour affichage des commentaires en JS

--- plugins/mel_forum/js/lib/comments.js
+++ plugins/mel_forum/js/lib/comments.js
@@ -1,3 +1,5 @@
+import { text } from "body-parser";
+
 export {PostComment, PostCommentView}
 
 class PostComment {
@@ -116,82 +118,79 @@ class PostComment {
     })
   }
 
-    // generateHtml(comments_array) {
-  //   let html = MelHtml.start;
-  //   html
-  //     .div({ class: 'comments-container mt-4' });
+  generateHtml() {
+    let html = MelHtml.start
+      .div({ 
+        id: 'comment_id-' + this.uid,
+        class: 'row comment'
+       })
+      .div({ class: 'col-12' })
+      .div({ class: 'd-flex align-items-center' })
+      .img({
+        src: 'this.##',
+        alt: 'Image de profil',
+        class: 'forum-comment-profile-image'
+       })
+       .span({ class: 'forum-content-author' })
+       .text(this.user_name)
+       .end('span')
+       .div({ class: 'forum-comment-date d-flex align-items-end' })
+       .i({ class: 'material-symbols-outlined' })
+       .text('access_time')
+       .end('i')
+       .span({ class: 'ml-1' })
+       .text(this.created)
+       .end('span')
+       .end('div')
+       .end('div')
+       .div({ class: 'forum-comment-text' })
+       .p(this.content)
+       .end('p')
+       .end('div')
+       .div({ class: 'forum-comment-reactions' })
+       .div({ class: 'reaction-item active mr-3' })
+       .i({ class: 'material-symbols-outlined' })
+       .text('thumb-up')
+       .end('i')
+       .span({ class: 'ml-2' })
+       .text(this.like)
+       .end('span')
+       .end('div')
+       .div({ class: 'reaction-item mr-3' })
+       .i({ class: 'material-symbols-outlined' })
+       .text('thumb-down')
+       .end('i')
+       .span({ class: 'ml-2' })
+       .text(this.dislike)
+       .end('span')
+       .end('div')
+       .div({ class: 'reaction-item mr-3 response' })
+       .i({ class: 'material-symbols-outlined' })
+       .text('mode_comment')
+       .end('i')
+       .span({ class: 'ml-2' })
+       .text('répondre')
+       .end('span')
+       .end('div')
+       .div({ class: 'reaction-item' })
+       .i({ class: 'material-symbols-outlined' })
+       .text('more_horiz')
+       .end('i')
+       .end('div')
+       .end('div')
+       .div({ class: 'forum-comment-responses' })
+       .i({ class: 'material-symbols-outlined' })
+       .text('arrow_drop_down')
+       .end('i')
+       .span({ class: 'ml-2' })
+       .text(this.nb_reponse)
+       .end('span')
+       .end('div')
+       .end('div')
+       .end('div');
 
-  //   comments_array(comment => {
-  //     //Commentaire
-  //     html
-  //       .div({ class: 'comment-item-custom ml-4' })
-  //       .div({ class: 'd-flex align-items-center' })
-  //       .img({
-  //         src: '',
-  //         alt: 'Image de Profil',
-  //         class: 'profile-image-custom'
-  //       })
-  //       .div({ class: 'ml-2' })
-  //       .span({ class: 'comment-author-custom' })
-  //       .text(creator)
-  //       .end('span')
-  //       .div({ class: 'd-flex align-items-center' })
-  //       .i({ class: 'material-symbols-outlined' })
-  //       .text('access_time')
-  //       .end('i')
-  //       .span({ class: 'ml-2' })
-  //       .text(date)
-  //       .end('span')
-  //       .end('div')
-  //       .end('div');
-
-  //     //Contenu du commentaire
-  //     html
-  //       .div({ class: 'comment-content' })
-  //       .p()
-  //       .text(content)
-  //       .end(p)
-  //       .end(div);
-
-  //     //Likes et nombre de réponses
-  //     html
-  //       .div({ class: 'Likes flex align-items-center mt-2' })
-  //       .div({ class: 'Like-item d-flex align-items-center mr-3' })
-  //       .i({ class: 'material-symbols-outlined' })
-  //       .text('thumb_up')
-  //       .end('i')
-  //       .span({ class: 'ml-2' })
-  //       .text(comment.likes)
-  //       .end('span')
-  //       .end('div')
-  //       .div({ class: 'Like-item d-flex align-items-center mr-3' })
-  //       .i({ class: 'material-symbols-outlined' })
-  //       .text('thumb_down')
-  //       .end('i')
-  //       .span({ class: 'ml-2' })
-  //       .text(comment.dislikes)
-  //       .end('span')
-  //       .end('div')
-  //       .div({ class: 'reaction-item d-flex align-items-center' })
-  //       .i({ class: 'material-symbols-outlined' })
-  //       .text('comment')
-  //       .end('i')
-  //       .span({ class: 'ml-2' })
-  //       .text(`x réponses`)
-  //       .end('span')
-  //       .i({ class: 'material-symbols-outlined' })
-  //       .text('keyboard_arrow_down')
-  //       .end('i')
-  //       .end('div')
-  //       .end('div');
-
-  //   }),
-
-  //     html
-  //       .end('div');
-
-  //   return html.generate();
-  // };
+    return html.generate();
+ }
 
 }
 

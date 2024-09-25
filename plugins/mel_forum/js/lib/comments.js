babc77d4ef73e330d1999611f236eb868fa10571
dcotton <dcotton>
2024-09-25 16:49:32 +0200

    fix(forum): Modification du code toggleMenu afin que le menu se ferme lorsqu'on clique en dehors de ce dernier ou qu'on sélectionne un item.

--- plugins/mel_forum/js/lib/comments.js
+++ plugins/mel_forum/js/lib/comments.js
@@ -196,7 +196,6 @@ async toggleResponses(id) {
  */
 
 async toggleReplyForm(uid, parentId) {
-  debugger;
   let form = $('#reply-form-' + uid);
   let isVisible = !form.hasClass('hidden');
   
@@ -350,13 +349,44 @@ async displaySingleComment(comment) {
  */
 toggleMenu(uid) {
   let selectContainer = $('#context-menu-' + uid);
+  let triggerButton = $('#trigger-' + uid);  // Bouton more_horiz
 
-  // Vérifier si le conteneur du select existe
+  // Vérifier si le conteneur du menu existe
   if (selectContainer.length) {
-    // Bascule l'affichage du conteneur
+    // Basculer l'affichage du conteneur
     selectContainer.toggleClass('hidden');
+
+    // Si le menu est visible, ajouter un écouteur pour détecter les clics extérieurs
+    if (!selectContainer.hasClass('hidden')) {
+      // Ajouter un écouteur de clic sur tout le document après un léger délai
+      setTimeout(() => {
+        $(document).on('click.menuOutside', function(event) {
+          // Vérifier si le clic est en dehors du menu et du bouton trigger
+          if (!$(event.target).closest(selectContainer).length && !$(event.target).closest(triggerButton).length) {
+            selectContainer.addClass('hidden');  // Masquer le menu
+            $(document).off('click.menuOutside'); // Retirer l'écouteur après fermeture
+          }
+        });
+
+        // Ajouter un écouteur d'événements pour chaque bouton du menu
+        selectContainer.find('.comment-options-button').on('click', function() {
+          selectContainer.addClass('hidden'); // Fermer le menu
+          $(document).off('click.menuOutside'); // Retirer l'écouteur après fermeture
+        });
+      }, 0);  // Délai de 0 pour que l'événement de clic sur le bouton soit géré en premier
+
+      // Empêcher la propagation du clic sur le bouton trigger pour éviter la fermeture immédiate
+      triggerButton.off('click').on('click', function(event) {
+        event.stopPropagation(); // Empêche la propagation du clic vers l'écouteur du document
+      });
+
+    } else {
+      // Si le menu est caché, retirer l'écouteur du document
+      $(document).off('click.menuOutside');
+    }
   }
 }
+
 
 /**
  * Basculer l'affichage entre le texte du commentaire et le champ de texte de modification.

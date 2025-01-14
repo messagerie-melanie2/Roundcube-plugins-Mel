4133548425ce1052fa9d0442d9afb60922ae423a
tdelphin <tdelphin>
2025-01-14 16:25:11 +0100

    documentation des espaces de travail

--- plugins/mel_workspace/js/lib/WorkspaceObject.js
+++ plugins/mel_workspace/js/lib/WorkspaceObject.js
@@ -12,8 +12,18 @@ import { MelObject } from '../../../mel_metapage/js/lib/mel_object.js';
 import { NavBarManager } from './navbar.generator.js';
 import { WorkspaceModuleBlock } from './WebComponents/workspace_module_block.js';
 
+export { WorkspaceObject, CurrentWorkspaceData };
+
 /**
+ * Contient les classes et fonctionnalités éssentiels pour une application lié à un espace.
  * @module Workspace/Object
+ * @local OnActionReceivedCallback
+ * @local MethodCallback
+ * @local WorkspaceObject
+ * @local CurrentWorkspaceData
+ * @local WorkspaceObjectData
+ * @local WorkspaceUser
+ * @local WorkspaceUsers
  */
 
 /**
@@ -27,16 +37,31 @@ import { WorkspaceModuleBlock } from './WebComponents/workspace_module_block.js'
  * @return {void}
  */
 
-export class WorkspaceObject extends MelObject {
+/**
+ * @class
+ * @classdesc Contient les données et fonctions utiles pour les classes/modules qui implémentent les espaces de travail
+ * @extends MelObject
+ * @abstract
+ * @hideconstructor
+ */
+class WorkspaceObject extends MelObject {
   constructor() {
     super();
   }
 
+  /**
+   * Contient le code de la fonction
+   * @virtual
+   * @override
+   */
   main() {
     super.main();
 
     /**
+     * Appelé lorsqu'une frame de l'espace envoie des données à l'espace via {@link WorkspaceObject.SendToWorkspace}
      * @type {BnumEvent<OnActionReceivedCallback>}
+     * @event
+     * @frommodule Workspace/Object {@linkto OnActionReceivedCallback}
      */
     this.onactionreceived = new BnumEvent();
 
@@ -45,16 +70,26 @@ export class WorkspaceObject extends MelObject {
     });
   }
 
+  /**
+   * Indique au module qu'il a fini son chargement
+   */
   loadModule() {
     this.main.loaded = true;
   }
 
+  /**
+   * Etat du module si il a fini de chargé ou non.
+   * @type {boolean}
+   * @readonly
+   */
   get loaded() {
     return this.main.loaded ?? false;
   }
 
   /**
+   * Données de l'espace en cours
    * @type {CurrentWorkspaceData}
+   * @readonly
    */
   get workspace() {
     return workspaceData.Instance;
@@ -95,11 +130,12 @@ export class WorkspaceObject extends MelObject {
   }
 
   /**
-   *
-   * @param {string} action
-   * @param {Object<string, string>} [params={}]
-   * @param {string} [type='GET']
+   * Effectue un appel api lié à la tâche des espaces de travail
+   * @param {string} action Action que l'on souhaite "taper".
+   * @param {Object<string, string>} [params={}] Paramètres de l'url
+   * @param {string} [type='GET'] Type d'appel
    * @returns {Promise<?any>}
+   * @async
    */
   async http_workspace_call(action, params = {}, type = 'GET') {
     params ??= {};
@@ -126,9 +162,9 @@ export class WorkspaceObject extends MelObject {
   }
 
   /**
-   *
-   * @param {string} action
-   * @param {Object<string, string>} [params={}]
+   * Effectue un appel api "POST" lié à la tâche des espaces de travail
+   * @param {string} action Action que l'on souhaite "taper".
+   * @param {Object<string, string>} [params={}] Paramètres de l'url
    * @returns {Promise<?any>}
    */
   async http_workspace_post(action, params = {}) {
@@ -136,9 +172,9 @@ export class WorkspaceObject extends MelObject {
   }
 
   /**
-   *
-   * @param {string} key
-   * @param {Object<string, string>} [params={}]
+   * Récupère un paramètre d'un espace de travail
+   * @param {string} key Clé du paramètre
+   * @param {Object<string, string>} [params={}] Paramètres de l'url
    * @returns {Promise<?any>}
    */
   async http_workspace_param_post(key, params = {}) {
@@ -173,6 +209,14 @@ export class WorkspaceObject extends MelObject {
     return node;
   }
 
+  /**
+   * Change l'état d'un module
+   * @param {string} task Module à changer d'état
+   * @param {boolean} state Nouvel état
+   * @param {WorkspaceModuleBlock} container Container du module
+   * @return {Promise<void>}
+   * @async
+   */
   async switchState(task, state, container) {
     if (state) this.hideBlock(container);
     else this.showBlock(container);
@@ -188,6 +232,11 @@ export class WorkspaceObject extends MelObject {
     );
   }
 
+  /**
+   * Vérifie si un des modules de l'espace est désactivé ou non
+   * @param {string} task Module à vérifier
+   * @returns {boolean}
+   */
   isDisabled(task) {
     return [true, 'true'].includes(
       this.get_env('workspace_modules_visibility')?.[task],
@@ -271,10 +320,25 @@ export class WorkspaceObject extends MelObject {
     return this;
   }
 
+  /**
+   * Récupère les données de l'espace en cours
+   * @returns {CurrentWorkspaceData}
+   * @static
+   */
   static GetWorkspaceData() {
     return workspaceData.Instance;
   }
 
+  /**
+   * Envoie des données à la frame parente
+   * @param {string} key Clé qui permettra de récupérer les données dans la frame parente
+   * @param {*} data Données à envoyer
+   * @param {Object} [options={}]
+   * @param {?string} [options.task=null] Ne pas utiliser.
+   * @returns {boolean} Si des messages ont été envoyé à une frame parente, alors `true` sera renvoyé.
+   * @static
+   * @todo Rendre la tâche utile
+   */
   static SendToParent(key, data, { task = null } = {}) {
     let rtn = false;
 
@@ -286,6 +350,16 @@ export class WorkspaceObject extends MelObject {
     return rtn;
   }
 
+  /**
+   * Envoie des données à la frame des espaces de travail
+   * @param {string} key
+   * @param {*} data Données à envoyer
+   * @param {Object} [options={}]
+   * @param {?string} [options.task=null] Ne pas utiliser.
+   * @returns {boolean} Si des messages ont été envoyé, alors `true` sera renvoyé.
+   * @static
+   * @todo Rendre la tâche utile
+   */
   static SendToWorkspace(key, data, { task = null } = {}) {
     const frame = FramesManager.Instance.get_frame('workspace', {
       jquery: false,
@@ -300,11 +374,24 @@ export class WorkspaceObject extends MelObject {
   }
 }
 
+/**
+ * @class
+ * @classdesc Objet qui contient les données envoyé par une frame enfante qui implémente un {@link WorkspaceObject}
+ * @package
+ */
 class WorkspaceObjectData {
   #workspace = null;
   #data = null;
   #context = null;
   #key = null;
+  /**
+   * Assigne les arguments aux variable privés
+   * @param {CurrentWorkspaceData} workspace Données de l'espace
+   * @param {string} key Clé de la données
+   * @param {Object} [options={}]
+   * @param {?any} [options.data=null] Données envoyer par la frame enfant
+   * @param {Window | undefined} [options.context=window] Context de la frame qui a envoyer les données
+   */
   constructor(workspace, key, { data = null, context = window } = {}) {
     this.#workspace = workspace;
     this.#data = data;
@@ -312,33 +399,67 @@ class WorkspaceObjectData {
     this.#key = key;
   }
 
+  /**
+   * Clé des données
+   * @type {string}
+   * @readonly
+   */
   get key() {
     return this.#key;
   }
 
+  /**
+   * Données de l'espace
+   * @type {CurrentWorkspaceData}
+   * @readonly
+   */
   get workspace() {
     return this.#workspace;
   }
 
+  /**
+   * Données envoyé par la frame enfant
+   * @type {?any}
+   * @readonly
+   */
   get data() {
     return this.#data;
   }
 
+  /**
+   * Context de la frame enfant
+   * @type {Window | undefined}
+   * @readonly
+   * @deprecated Ne fonctionne pas
+   */
   get context() {
     return this.#context;
   }
 
+  /**
+   * Fonctions d'aide
+   * @type {EmptyMelObject}
+   * @readonly
+   */
   get helper() {
     return MelObject.Empty();
   }
 }
 
+/**
+ * @class
+ * @classdesc Données d'un utilisateur
+ * @package
+ */
 class WorkspaceUser {
   #email = null;
   #name = null;
   #fullname = null;
   #external = false;
-
+  /**
+   * Fractionne à partir d'une donnée brute
+   * @param {{email:string, name:string, fullname:string, is_external:boolean}} user Utilisateur brute à convertir en données structurée
+   */
   constructor(user) {
     this.#email = user.email;
     this.#name = user.name;
@@ -346,31 +467,54 @@ class WorkspaceUser {
     this.#external = user.is_external;
   }
 
+  /**
+   * Email de l'utilisateur
+   * @type {string}
+   * @readonly
+   */
   get email() {
     return this.#email;
   }
 
   /**
+   * Nom de l'utilisateur
    * @type {string}
+   * @readonly
    */
   get name() {
     return this.#name;
   }
 
+  /**
+   * Nom complet de l'utilisateur
+   * @type {string}
+   * @readonly
+   */
   get fullname() {
     return this.#fullname;
   }
 
+  /**
+   * Si il s'agit d'un utilisateur externe ou non
+   * @type {boolean}
+   * @readonly
+   */
   get external() {
     return this.#external;
   }
 }
 
+/**
+ * @class
+ * @classdesc Liste des utilisateurs d'un espace.
+ * @package
+ */
 class WorkspaceUsers {
   #users = {};
   /**
-   *
+   * Récupère la liste des utilisateurs
    * @param  {...WorkspaceUser} users
+   * @frommoduleparam  Workspace/Object users {@linkto WorkspaceUser}
    */
   constructor(...users) {
     for (const element of users) {
@@ -378,45 +522,73 @@ class WorkspaceUsers {
     }
   }
 
+  /**
+   * Liste des adresses emails des utilisateurs
+   * @type {string[]}
+   * @readonly
+   */
   get emails() {
     return Object.keys(this.#users);
   }
 
   /**
-   *
+   * Récupère les utilisateurs sous la forme d'un tableau
    * @returns {WorkspaceUser[]}
    */
   toArray() {
     return [...this];
   }
 
+  /**
+   * Convertit la liste des utilisateurs en énumerable
+   * @returns {MelEnumerable}
+   * @frommodulereturn MelLinq
+   */
   toEnumerable() {
     return new MelEnumerable(this.generator.bind(this));
   }
 
   /**
-   *
-   * @param {*} email
+   * Récupère Un utilisateur à partir de son email
+   * @param {string} email
    * @returns {WorkspaceUser}
    */
   get(email) {
     return this.#users[email];
   }
 
+  /**
+   * Enumère les utilisateurs
+   * @yields {WorkspaceUser}
+   */
   *generator() {
     for (const key of Object.keys(this.#users)) {
       yield this.#users[key];
     }
   }
 
+  /**
+   * Enumère les utilisateurs
+   * @yields {WorkspaceUser}
+   */
   *[Symbol.iterator]() {
     yield* this.generator();
   }
 }
 
-export class CurrentWorkspaceData {
+/**
+ * @class
+ * @classdesc Données de l'espace
+ * @package
+ */
+class CurrentWorkspaceData {
   #users = null;
   #workspace_created = null;
+  /**
+   * Constructeur de la classe.
+   *
+   * Parse "current_workspace_services_actives" en objet si c'est une chaîne de charactères.
+   */
   constructor() {
     if (typeof rcmail.env.current_workspace_services_actives === 'string')
       rcmail.env.current_workspace_services_actives = JSON.parse(
@@ -424,16 +596,28 @@ export class CurrentWorkspaceData {
       );
   }
 
+  /**
+   * Id de l'espace de travail
+   * @type {string}
+   * @readonly
+   */
   get uid() {
     return rcmail.env.current_workspace_uid;
   }
 
+  /**
+   * Titre de l'espace
+   * @type {string}
+   * @readonly
+   */
   get title() {
     return rcmail.env.current_workspace_title;
   }
 
   /**
+   * Utilisateurs de l'espace
    * @type {WorkspaceUsers}
+   * @readonly
    */
   get users() {
     if (!this.#users) {
@@ -446,18 +630,38 @@ export class CurrentWorkspaceData {
     return this.#users;
   }
 
+  /**
+   * Si l'espace est public ou non
+   * @type {boolean}
+   * @readonly
+   */
   get isPublic() {
     return rcmail.env.current_workspace_is_public;
   }
 
+  /**
+   * Couleur de l'espace
+   * @type {string}
+   * @readonly
+   */
   get color() {
     return rcmail.env.current_workspace_color;
   }
 
+  /**
+   * Liste des services de l'espace
+   * @type {Object<string, string | Object<string, any> | boolean | number>}
+   * @readonly
+   */
   get services() {
     return rcmail.env.current_workspace_services_actives;
   }
 
+  /**
+   * Date de création de l'espace
+   * @type {external:moment}
+   * @readonly
+   */
   get created() {
     if (!this.#workspace_created)
       this.#workspace_created = moment(rcmail.env.current_workspace_created);
@@ -465,6 +669,11 @@ export class CurrentWorkspaceData {
     return this.#workspace_created;
   }
 
+  /**
+   * Récupère l'information si une application est chargée ou non
+   * @param {string} service
+   * @returns {boolean}
+   */
   app_loaded(service) {
     return (
       this.services[service] &&
@@ -477,10 +686,20 @@ export class CurrentWorkspaceData {
 }
 
 /**
+ * Singleton des données de l'espace pour éviter de recréer un objet à chaque fois.
  * @type {WrapperObject<CurrentWorkspaceData>}
+ * @constant
+ * @package
+ * @frommodule Workspace/Object {@linkto CurrentWorkspaceData}
  */
 const workspaceData = new WrapperObject(CurrentWorkspaceData);
 
+/**
+ * Propriété de l'objet "window" à ajouter pour indiquer que le listener "message" à déjà été ajouter.
+ * @type {string}
+ * @constant
+ * @package
+ */
 const window_prop_data = 'wspobjlsn';
 if (!window[window_prop_data]) {
   window.addEventListener(

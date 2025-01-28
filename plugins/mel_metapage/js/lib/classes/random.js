b95056bea724b665a42ef114c1b76e89d784b21b
tdelphin <tdelphin>
2025-01-28 11:10:30 +0100

    adding random string uppercase + numbers

--- plugins/mel_metapage/js/lib/classes/random.js
+++ plugins/mel_metapage/js/lib/classes/random.js
@@ -15,13 +15,32 @@ export class Random {
     return `rgb(${Random.intRange(0, 255)}, ${Random.intRange(0, 255)}, ${Random.intRange(0, 255)})`;
   }
 
-  static random_string(size) {
+  static random_string(
+    size,
+    { includeNumbers = false, includeUpperCase = false } = {},
+  ) {
     const ALPHA = 'abcdefghijklmnopqrstuvwxyz';
 
     let str = EMPTY_STRING;
 
-    for (let index = 0; index < size; ++index) {
-      str += ALPHA[this.intRange(0, ALPHA.length)];
+    for (let index = 0, char; index < size; ++index) {
+      switch (!includeNumbers && !includeUpperCase ? 3 : this.intRange(0, 3)) {
+        case 0:
+          char = ALPHA[this.intRange(0, ALPHA.length)];
+          if (includeUpperCase) char = char.toUpperCase();
+
+          str += char;
+          break;
+
+        case 1:
+          if (includeNumbers) str += this.intRange(0, 10).toString();
+          else str += ALPHA[this.intRange(0, ALPHA.length)];
+          break;
+
+        default:
+          str += ALPHA[this.intRange(0, ALPHA.length)];
+          break;
+      }
     }
 
     return str;

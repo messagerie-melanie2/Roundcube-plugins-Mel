50c945e7f0658671869004a0486775eba9fe4b31
Rotomeca <delphin.tommy@gmail.com>
2025-03-06 10:50:04 +0100

    feat(webcomp): Démarrer `HTMLBnumButton`

---
+++ plugins/mel_metapage/js/lib/html/JsHtml/CustomAttributes/button/FormComponent.js
@@ -0,0 +1,88 @@
+import { EMPTY_STRING, SPACE } from '../../../../constants/constants';
+import AHTMLComponent from '../lib/AHTMLComponent';
+
+export const ENABLE_CLASS_BUTTON = true;
+export const CLASS_BUTTON = 'mel-button';
+export const OLD_BNUM_MODE = true;
+
+export default class FormComponent extends AHTMLComponent {
+  /**
+   * @type {import('../lib/AHTMLCustomInternalElement').default}
+   */
+  #_parent;
+  /**
+   *
+   * @param {import('../lib/AHTMLCustomInternalElement').default} parent
+   */
+  constructor(parent) {
+    super(parent);
+    this.#_parent = parent;
+  }
+
+  /**
+   * @type {EButtonType}
+   * @readonly
+   */
+  get variation() {
+    return EButtonType.fromString(
+      this.getData('variation') || EButtonType.fromString(EButtonType.primary),
+    );
+  }
+
+  setup() {
+    super.setup();
+
+    let variationClass = [];
+
+    if (ENABLE_CLASS_BUTTON) variationClass.push(CLASS_BUTTON);
+
+    switch (this.variation) {
+      case EButtonType.primary:
+        variationClass.push(OLD_BNUM_MODE ? EMPTY_STRING : '--primary');
+        this.#_parent.setState('primary');
+        break;
+
+      case EButtonType.secondary:
+        variationClass.push(OLD_BNUM_MODE ? 'white' : '--secondary');
+        this.#_parent.setState('secondary');
+        break;
+
+      case EButtonType.danger:
+        variationClass.push(OLD_BNUM_MODE ? 'btn btn-danger' : '--danger');
+        this.#_parent.setState('danger');
+        break;
+
+      default:
+        throw new Error('Variation pas géré');
+    }
+
+    this.#_parent.addClass(
+      ...variationClass
+        .filter((x) => x !== EMPTY_STRING)
+        .map((x) => (x.includes(SPACE) ? x.split(SPACE) : x))
+        .flat(),
+    );
+  }
+}
+
+/**
+ * @enum {Symbol}
+ * @property {Symbol} primary
+ * @property {Symbol} secondary
+ * @property {Symbol} danger
+ * @property {(sym:Symbol) => string} toString
+ * @property {(str:string) => Symbol} fromString
+ */
+export const EButtonType = Object.freeze({
+  primary: Symbol('primary'),
+  secondary: Symbol('secondary'),
+  danger: Symbol('danger'),
+  toString(sym) {
+    return Object.keys(this)
+      .filter((x) => typeof EButtonType[x] === 'symbol')
+      .find((x) => EButtonType[x] === sym);
+  },
+  fromString(str) {
+    return EButtonType[str];
+  },
+});

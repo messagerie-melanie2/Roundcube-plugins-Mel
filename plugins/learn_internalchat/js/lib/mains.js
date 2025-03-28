0e583ebc508bdaf6436df4bcee444414376545a1
Rotomeca <delphin.tommy@gmail.com>
2025-03-28 11:18:21 +0100

    structure

---
+++ plugins/learn_internalchat/js/lib/mains.js
@@ -0,0 +1,13 @@
+import { MelObject } from '../../../mel_metapage/js/lib/mel_object.js';
+
+export default class Main extends MelObject {
+  constructor() {
+    super();
+  }
+
+  main() {
+    super.main();
+  }
+}
+
+new Main();
--- plugins/learn_internalchat/learn_internalchat.php
+++ plugins/learn_internalchat/learn_internalchat.php
@@ -14,15 +14,35 @@ class learn_internalchat extends bnum_plugin {
         $this->add_texts('localization/', $addToJavascript);
       }
 
-      $this->register_action('index', 'action_index');
+      $this->register_action('index', [$this, 'action_index']);
     }
+
+    $this->add_button([
+      'href' => '?_task='. self::TASK_NAME,
+      'class'	=> self::TASK_NAME,
+      'classsel' => 'button-selected'. self::TASK_NAME,
+      'innerclass' => 'inner',
+      'label'	=> 'private_chat',
+      'title' => 'private_chat',
+      'type'       => 'link',
+      'domain' => "learn_internalchat"
+    ], 'taskbar');
   }
 
   public function action_index() : void {
     $this->include_module('main.js', 'js/lib');
-    $this->include_css('style.css');
+    $this->include_css('index.css');
 
     $this->rc()->output->set_env('max_user', $this->get_config('max_user'));
-    $this->rc()->output->send('learn_internal_chat.index');
+    $this->rc()->output->send('learn_internalchat.index');
+  }
+
+  public function action_contact_user() : void {
+    $userToContact = $this->get_input('_user', rcube_utils::INPUT_GET);
+
+    if (strpos($userToContact, '@') !== false) $userToContact = $this->get_user_from_email($userToContact);
+    else $userToContact = $this->get_user($userToContact);
+
+    $this->sendEncodedExit($userToContact !== null);
   }
 }

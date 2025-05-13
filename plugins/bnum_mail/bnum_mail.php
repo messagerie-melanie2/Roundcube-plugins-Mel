0683e40fdf61ddb97b9a1f403accee75c04e2012
Rotomeca <delphin.tommy@gmail.com>
2025-05-13 16:20:36 +0200

    fix(mails): :bug: Changement au niveau des colonnes, la prioritée est passé en config au lieu de modifier les sources

--- plugins/bnum_mail/bnum_mail.php
+++ plugins/bnum_mail/bnum_mail.php
@@ -10,5 +10,16 @@ class bnum_mail extends bnum_plugin {
 
         if (isset($config) && !$this->rc()->output->get_env('search_scope')) $this->rc()->output->set_env('search_scope', $config);
     }
+
+    $this->add_hook('messages_list', [$this, 'hook_message_list']);
+  }
+
+  public function hook_message_list($args) {
+    if ($args['cols'] && is_array($args['cols'])) {
+      // Gestion des colonnes additionnels
+      $args['cols'] = array_merge($args['cols'], $this->get_config('additional_columns', []));
+    }
+
+    return $args;
   }
 }

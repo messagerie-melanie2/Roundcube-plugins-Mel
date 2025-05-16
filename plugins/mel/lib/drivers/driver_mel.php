2b780e0754fed71b0489b5c1868ba840b9d8cae4
dcotton <dcotton>
2025-05-16 14:28:16 +0200

    Revert "fix: :bug: mise a jour variable pour connexion au bnum.local"
    This reverts commit f33ef6e77e0ec653bf3f151b39853b98e33bda2b.

--- plugins/mel/lib/drivers/driver_mel.php
+++ plugins/mel/lib/drivers/driver_mel.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Plugin Mél
  *
@@ -19,43 +18,42 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-abstract class driver_mel
-{
+abstract class driver_mel {
   /**
    * Label utilisé dans les boites partagées pour l'arborescence des dossiers
    *
    * @var string
    */
   protected $BALP_LABEL = null;
-
+  
   /**
    * Dossier pour les brouillons
    *
    * @var string
    */
   protected $MBOX_DRAFT = null;
-
+  
   /**
    * Dossier pour les éléments envoyés
    *
    * @var string
    */
   protected $MBOX_SENT = null;
-
+  
   /**
    * Dossier pour les indésirables
    *
    * @var string
    */
   protected $MBOX_JUNK = null;
-
+  
   /**
    * Dossier pour la corbeille
    *
    * @var string
    */
   protected $MBOX_TRASH = null;
-
+  
   /**
    * Singleton
    *
@@ -69,7 +67,7 @@ abstract class driver_mel
    * @var \LibMelanie\Api\Defaut\User[]
    */
   protected static $_users;
-
+  
   /**
    * Singleton for the groups
    * 
@@ -87,8 +85,7 @@ abstract class driver_mel
    *
    * @return driver_mel
    */
-  public static function get_instance()
-  {
+  public static function get_instance() {
     if (!isset(self::$driver)) {
       $drivername = strtolower(rcmail::get_instance()->config->get('mel_driver', 'mce'));
       require_once $drivername . '/' . $drivername . '.php';
@@ -103,8 +100,7 @@ abstract class driver_mel
    *
    * @return driver_mel
    */
-  public static function gi()
-  {
+  public static function gi() {
     return self::get_instance();
   }
 
@@ -116,8 +112,7 @@ abstract class driver_mel
    * 
    * @return staticClass object of the choosen type
    */
-  protected function object($objectName, $params = [])
-  {
+  protected function object($objectName, $params = []) {
     $class = new \ReflectionClass(static::$_objectsNS . $objectName);
     return $class->newInstanceArgs($params);
   }
@@ -129,8 +124,7 @@ abstract class driver_mel
    * 
    * @return \LibMelanie\Api\Defaut\User
    */
-  public function user($params = [])
-  {
+  public function user($params = []) {
     return $this->object('User', $params);
   }
 
@@ -141,8 +135,7 @@ abstract class driver_mel
    * 
    * @return \LibMelanie\Api\Defaut\UserPrefs
    */
-  public function userprefs($params = [])
-  {
+  public function userprefs($params = []) {
     return $this->object('UserPrefs', $params);
   }
 
@@ -153,8 +146,7 @@ abstract class driver_mel
    * 
    * @return \LibMelanie\Api\Defaut\Member
    */
-  public function member($params = [])
-  {
+  public function member($params = []) {
     return $this->object('Member', $params);
   }
 
@@ -165,8 +157,7 @@ abstract class driver_mel
    * 
    * @return \LibMelanie\Api\Defaut\Users\Outofoffice
    */
-  public function users_outofoffice($params = [])
-  {
+  public function users_outofoffice($params = []) {
     return $this->object('Users\\Outofoffice', $params);
   }
 
@@ -177,8 +168,7 @@ abstract class driver_mel
    * 
    * @return \LibMelanie\Api\Defaut\Users\Type
    */
-  public function users_type($params = [])
-  {
+  public function users_type($params = []) {
     return $this->object('Users\\Type', $params);
   }
 
@@ -189,8 +179,7 @@ abstract class driver_mel
    * 
    * @return \LibMelanie\Api\Defaut\Users\Share
    */
-  public function users_share($params = [])
-  {
+  public function users_share($params = []) {
     return $this->object('Users\\Share', $params);
   }
 
@@ -201,8 +190,7 @@ abstract class driver_mel
    * 
    * @return \LibMelanie\Api\Defaut\Group
    */
-  public function group($params = [])
-  {
+  public function group($params = []) {
     return $this->object('Group', $params);
   }
 
@@ -213,8 +201,7 @@ abstract class driver_mel
    * 
    * @return \LibMelanie\Api\Defaut\Share
    */
-  public function share($params = [])
-  {
+  public function share($params = []) {
     return $this->object('Share', $params);
   }
 
@@ -225,8 +212,7 @@ abstract class driver_mel
    * 
    * @return \LibMelanie\Api\Defaut\Calendar
    */
-  public function calendar($params = [])
-  {
+  public function calendar($params = []) {
     return $this->object('Calendar', $params);
   }
 
@@ -237,8 +223,7 @@ abstract class driver_mel
    * 
    * @return \LibMelanie\Api\Defaut\Event
    */
-  public function event($params = [])
-  {
+  public function event($params = []) {
     return $this->object('Event', $params);
   }
 
@@ -249,8 +234,7 @@ abstract class driver_mel
    * 
    * @return \LibMelanie\Api\Defaut\Exception
    */
-  public function exception($params = [])
-  {
+  public function exception($params = []) {
     return $this->object('Exception', $params);
   }
 
@@ -261,8 +245,7 @@ abstract class driver_mel
    * 
    * @return \LibMelanie\Api\Defaut\Organizer
    */
-  public function organizer($params = [])
-  {
+  public function organizer($params = []) {
     return $this->object('Organizer', $params);
   }
 
@@ -273,8 +256,7 @@ abstract class driver_mel
    * 
    * @return \LibMelanie\Api\Defaut\Attendee
    */
-  public function attendee($params = [])
-  {
+  public function attendee($params = []) {
     return $this->object('Attendee', $params);
   }
 
@@ -285,8 +267,7 @@ abstract class driver_mel
    * 
    * @return \LibMelanie\Api\Defaut\Attachment
    */
-  public function attachment($params = [])
-  {
+  public function attachment($params = []) {
     return $this->object('Attachment', $params);
   }
 
@@ -297,8 +278,7 @@ abstract class driver_mel
    * 
    * @return \LibMelanie\Api\Defaut\Recurrence
    */
-  public function recurrence($params = [])
-  {
+  public function recurrence($params = []) {
     return $this->object('Recurrence', $params);
   }
 
@@ -309,8 +289,7 @@ abstract class driver_mel
    * 
    * @return \LibMelanie\Api\Defaut\Addressbook
    */
-  public function addressbook($params = [])
-  {
+  public function addressbook($params = []) {
     return $this->object('Addressbook', $params);
   }
 
@@ -321,8 +300,7 @@ abstract class driver_mel
    * 
    * @return \LibMelanie\Api\Defaut\Contact
    */
-  public function contact($params = [])
-  {
+  public function contact($params = []) {
     return $this->object('Contact', $params);
   }
 
@@ -333,8 +311,7 @@ abstract class driver_mel
    * 
    * @return \LibMelanie\Api\Defaut\Taskslist
    */
-  public function taskslist($params = [])
-  {
+  public function taskslist($params = []) {
     return $this->object('Taskslist', $params);
   }
 
@@ -345,8 +322,7 @@ abstract class driver_mel
    * 
    * @return \LibMelanie\Api\Defaut\Task
    */
-  public function task($params = [])
-  {
+  public function task($params = []) {
     return $this->object('Task', $params);
   }
 
@@ -357,8 +333,7 @@ abstract class driver_mel
    * 
    * @return \LibMelanie\Api\Defaut\Workspace
    */
-  public function workspace($params = [])
-  {
+  public function workspace($params = []) {
     return $this->object('Workspace', $params);
   }
 
@@ -369,8 +344,7 @@ abstract class driver_mel
    * 
    * @return \LibMelanie\Api\Defaut\Workspaces\Share
    */
-  public function workspace_share($params = [])
-  {
+  public function workspace_share($params = []) {
     return $this->object('Workspaces\\Share', $params);
   }
 
@@ -381,8 +355,7 @@ abstract class driver_mel
    * 
    * @return \LibMelanie\Api\Defaut\Workspaces\Share
    */
-  public function workspace_hashtag($params = [])
-  {
+  public function workspace_hashtag($params = []) {
     return $this->object('Workspaces\\Hashtag', $params);
   }
 
@@ -393,8 +366,7 @@ abstract class driver_mel
    * 
    * @return \LibMelanie\Api\Defaut\News\News
    */
-  public function news($params = [])
-  {
+  public function news($params = []) {
     return $this->object('News\\News', $params);
   }
 
@@ -405,8 +377,7 @@ abstract class driver_mel
    * 
    * @return \LibMelanie\Api\Defaut\News\Rss
    */
-  public function rss($params = [])
-  {
+  public function rss($params = []) {
     return $this->object('News\\Rss', $params);
   }
 
@@ -417,8 +388,7 @@ abstract class driver_mel
    * 
    * @return \LibMelanie\Api\Defaut\News\NewsShare
    */
-  public function newsshare($params = [])
-  {
+  public function newsshare($params = []) {
     return $this->object('News\\NewsShare', $params);
   }
 
@@ -429,8 +399,7 @@ abstract class driver_mel
    * 
    * @return \LibMelanie\Api\Defaut\Notification
    */
-  public function notification($params = [])
-  {
+  public function notification($params = []) {
     return $this->object('Notification', $params);
   }
 
@@ -439,8 +408,7 @@ abstract class driver_mel
    * 
    * @return string DELIMITER
    */
-  public function objectShareDelimiter()
-  {
+  public function objectShareDelimiter() {
     return constant(static::$_objectsNS . 'ObjectShare::DELIMITER');
   }
 
@@ -449,24 +417,26 @@ abstract class driver_mel
    * 
    * @return boolean
    */
-  public function isSsl($host = null)
-  {
+  public function isSsl($host = null) {
     $isSsl = false;
     $config = rcmail::get_instance()->config->get('imap_host', null);
     if (isset($host) && is_array($config)) {
       if (in_array($host, $config)) {
         $isSsl = false;
-      } else if (in_array('ssl://' . $host, $config)) {
+      }
+      else if (in_array('ssl://'.$host, $config)) {
         $isSsl = true;
       }
-    } else if (is_array($config) && strpos($config[0], 'ssl://') === 0) {
+    }
+    else if (is_array($config) && strpos($config[0], 'ssl://') === 0) {
       $isSsl = true;
-    } else if (is_string($config) && strpos($config, 'ssl://') === 0) {
+    }
+    else if (is_string($config) && strpos($config, 'ssl://') === 0) {
       $isSsl = true;
     }
     return $isSsl;
   }
-
+  
   /**
    * Retourne l'objet User associé à l'utilisateur courant
    * Permet de retourner l'instance User en fonction du driver
@@ -494,7 +464,7 @@ abstract class driver_mel
    * @return \LibMelanie\Api\Defaut\Group
    */
   abstract public function getGroup($group_dn = null, $load = true, $fromCache = true, $itemName = null);
-
+  
   /**
    * Retourne le MBOX par defaut pour une boite partagée donnée
    * Peut être INBOX ou autre chose si besoin
@@ -503,7 +473,7 @@ abstract class driver_mel
    * @return string $mbox par defaut
    */
   abstract public function getMboxFromBalp($balpname);
-
+  
   /**
    * Récupère et traite les infos de routage depuis l'objet LDAP 
    * pour retourner le hostname de connexion IMAP et/ou SMTP
@@ -514,7 +484,7 @@ abstract class driver_mel
    * @return string $hostname de routage, null si pas de routage trouvé
    */
   abstract public function getRoutage($infos, $function = '');
-
+  
   /**
    * Positionne des headers pour un message avant de l'envoyer
    *
@@ -522,14 +492,14 @@ abstract class driver_mel
    * @return array $headers Retourne les headers completes
    */
   abstract public function setHeadersMessageBeforeSend($headers);
-
+  
   /**
    * Est-ce que l'utilisateur courant a le droit d'accéder au stockage
    *
    * @return boolean true si le stockage doit être affiché, false sinon
    */
   abstract public function userHasAccessToStockage();
-
+  
   /**
    * Est-ce que le mot de passe de l'utilisateur doit changer
    * Si c'est le cas la page de changement de mot de passe sera affichée après le login
@@ -664,31 +634,26 @@ abstract class driver_mel
    * 
    * @return string $uuid
    */
-  protected function uuidv4()
-  {
-    return sprintf(
-      '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-
+  protected function uuidv4() {
+    return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+  
       // 32 bits for "time_low"
-      mt_rand(0, 0xffff),
-      mt_rand(0, 0xffff),
-
+      mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+  
       // 16 bits for "time_mid"
       mt_rand(0, 0xffff),
-
+  
       // 16 bits for "time_hi_and_version",
       // four most significant bits holds version number 4
       mt_rand(0, 0x0fff) | 0x4000,
-
+  
       // 16 bits, 8 bits for "clk_seq_hi_res",
       // 8 bits for "clk_seq_low",
       // two most significant bits holds zero and one for variant DCE1.1
       mt_rand(0, 0x3fff) | 0x8000,
-
+  
       // 48 bits for "node"
-      mt_rand(0, 0xffff),
-      mt_rand(0, 0xffff),
-      mt_rand(0, 0xffff)
+      mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
     );
   }
 
@@ -702,8 +667,7 @@ abstract class driver_mel
    * 
    * @return string Identifiant au format MCE
    */
-  public function rcToMceId($rcId)
-  {
+  public function rcToMceId($rcId) {
     return str_replace(['_-P-_', '_-A-_', '_-C-_'], ['.', '@', '%'], $rcId);
   }
 
@@ -717,8 +681,7 @@ abstract class driver_mel
    * 
    * @return string Identifiant au format Roundcube
    */
-  public function mceToRcId($mceId)
-  {
+  public function mceToRcId($mceId) {
     return str_replace(['.', '@', '%'], ['_-P-_', '_-A-_', '_-C-_'], $mceId);
   }
 
@@ -728,8 +691,7 @@ abstract class driver_mel
    * @param string $mbox
    * @return array Identité
    */
-  public function getIdentityFromMbox($mbox)
-  {
+  public function getIdentityFromMbox($mbox) {
     if (strpos($mbox, $this->BALP_LABEL) === 0) {
       $delimiter = rcmail::get_instance()->get_storage()->delimiter;
       foreach (rcmail::get_instance()->user->list_identities() as $id) {
@@ -748,59 +710,54 @@ abstract class driver_mel
     }
     return $identity;
   }
-
+  
   /**
    * Retourne le label des balp dans l'arborescence de fichiers IMAP
    * Permet de redéfinir l'affichage des dossiers pour l'utilisateur
    *
    * @return string Le label ou null si pas nécessaire
    */
-  public function getBalpLabel()
-  {
+  public function getBalpLabel() {
     return $this->BALP_LABEL;
   }
-
+  
   /**
    * Retourne le dossier pour les brouillons
    * Peut être null, le plugin mel prendra alors la configuration Roundcube
    *
    * @return string
    */
-  public function getMboxDraft()
-  {
+  public function getMboxDraft() {
     return $this->MBOX_DRAFT;
   }
-
+  
   /**
    * Retourne le dossier pour les éléments envoyés
    * Peut être null, le plugin mel prendra alors la configuration Roundcube
    *
    * @return string
    */
-  public function getMboxSent()
-  {
+  public function getMboxSent() {
     return $this->MBOX_SENT;
   }
-
+  
   /**
    * Retourne le dossier pour les indésirables
    * Peut être null, le plugin mel prendra alors la configuration Roundcube
    *
    * @return string
    */
-  public function getMboxJunk()
-  {
+  public function getMboxJunk() {
     return $this->MBOX_JUNK;
   }
-
+  
   /**
    * Retourne le dossier pour la corbeille
    * Peut être null, le plugin mel prendra alors la configuration Roundcube
    *
    * @return string
    */
-  public function getMboxTrash()
-  {
+  public function getMboxTrash() {
     return $this->MBOX_TRASH;
   }
 
@@ -814,13 +771,12 @@ abstract class driver_mel
    * @param string $mail Mail à traiter peut être un objet de partage ou non, ou un mailroutingaddress
    * @return array(($user_objet_share, $user_host, $user_bal)
    */
-  public function getShareUserBalpHostFromMail($mail)
-  {
-    $user_objet_share = $user_host = $user_bal = null;
+  public function getShareUserBalpHostFromMail($mail) {
+    $user_objet_share=$user_host=$user_bal=null;
     // Split sur @ pour les comptes de boites partagées <username>@<hostname>
     $inf = explode('@', $mail, 2);
     // Le username est encodé pour éviter les problèmes avec @
-    $user_objet_share = $user_bal = $inf[0] ? urldecode($inf[0]) : null;
+    $user_objet_share = $user_bal = $inf[0]?urldecode($inf[0]):null;
     // Récupération du host
     $user_host = $inf[1] ?: null;
     return [$user_objet_share, $user_host, $user_bal];
@@ -835,9 +791,8 @@ abstract class driver_mel
    *
    * @return array ($user_objet_share, $user_host, $user_bal)
    */
-  public function getShareUserBalpHostFromSession()
-  {
-    $user_objet_share = $user_host = $user_bal = null;
+  public function getShareUserBalpHostFromSession() {
+    $user_objet_share=$user_host=$user_bal=null;
     $rc = rcmail::get_instance();
     $user_objet_share = $user_bal = $rc->user->get_username('local');
     $user_host = $rc->user->get_username('host');
@@ -848,9 +803,8 @@ abstract class driver_mel
    *  Récupère un couple login/password pour jouer une authentification externe de type basic
    * @return array [login,password] for authentification type basic
    */
-  public function getBasicAuth()
-  {
+  public function getBasicAuth() {
     $rc = rcmail::get_instance();
-    return [$rc->user->get_username(), $rc->get_user_password()];
+    return [$rc->user->get_username(),$rc->get_user_password()];
   }
 }

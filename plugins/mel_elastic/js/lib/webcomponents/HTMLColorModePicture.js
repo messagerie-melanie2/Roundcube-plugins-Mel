198b3f71ed750d0dacadd6a6430ed2cb4588470a
Rotomeca <delphin.tommy@gmail.com>
2025-08-19 09:39:36 +0200

    style(webcomponents): :wastebasket: `HTMLColorModePicture` est déprécié
    Utilisez plutôt `BnumHTMLPicture`.

--- plugins/mel_elastic/js/lib/webcomponents/HTMLColorModePicture.js
+++ plugins/mel_elastic/js/lib/webcomponents/HTMLColorModePicture.js
@@ -1,4 +1,9 @@
-import BnumHTMLPicture from './classes/BnumHTMLPicture.js';
+import BnumHTMLPicture from "./classes/BnumHTMLPicture.js";
 
+/**
+ * Élément web personnalisé permettant d'afficher une image qui s'adapte automatiquement au mode sombre ou clair de l'interface.
+ * @see {@link BnumHTMLPicture} pour plus de détails
+ * @deprecated Utilisez {@link BnumHTMLPicture} à la place.
+ */
 const HTMLColorModePicture = BnumHTMLPicture;
 export default HTMLColorModePicture;

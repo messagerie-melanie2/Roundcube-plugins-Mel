74266b6c0f6498da5b56a486a53965526def96be
Rotomeca <delphin.tommy@gmail.com>
2025-11-17 11:52:19 +0100

    docs(config): :memo: Ajout de la documentation

--- plugins/mel_supervision/mel_supervision.php
+++ plugins/mel_supervision/mel_supervision.php
@@ -3,21 +3,60 @@ if (!defined('EMPTY_STRING')) {
     define('EMPTY_STRING', '');
 }
 
+/**
+ * Plugin de supervision des modifications d'identité utilisateur.
+ */
 class mel_supervision extends bnum_plugin {
+  /**
+   * Nom du plugin requis pour fonctionner.
+   */
   const REQUIRED_PLUGIN = 'mel_helper';
+  /**
+   * Hook surveillé pour la mise à jour d'identité.
+   */
   const HOOK_IDENTITY_UPDATE = 'identity_update';
+  /**
+   * Nom de la fonction callback pour le hook.
+   */
   const CALLBACK_IDENTITY_UPDATE = 'hook_identity_update';
+  /**
+   * Dossier contenant les textes de localisation.
+   */
   const FOLDER_LOCALIZATION = 'localization';
+  /**
+   * Champ "reply-to" surveillé.
+   */
   const FIELD_TO = 'reply-to';
+  /**
+   * Champ "bcc" surveillé.
+   */
   const FIELD_BCC = 'bcc';
+  /**
+   * Expéditeur des mails d'avertissement.
+   */
   const MAIL_SENDER = 'bnum';
+  /**
+   * Destinataires des alertes.
+   */
   const CONFIG_MAIL_RECEIVER = 'mail_warning_emails';
+  /**
+   * Option pour ajouter les administrateurs Amédée en copie.
+   */
   const CONFIG_AMEDEE_ADMIN = 'mail_warning_amedee_admin';
+  /**
+   * Clé du sujet du mail d'avertissement.
+   */
   const TEXT_MAIL_SUBJECT = 'mail_warning_subject';
+  /**
+   * Clé du corps du mail d'avertissement.
+   */
   const TEXT_MAIL_BODY = 'mail_warning_message';
 
   public $task = 'settings';
 
+  /**
+   * Initialise le plugin et ajoute les hooks nécessaires.
+   */
   function init() {
     $this->require_plugin(self::REQUIRED_PLUGIN);
     $this->add_hooks(
@@ -27,6 +66,13 @@ class mel_supervision extends bnum_plugin {
     );
   }
 
+  /**
+   * Hook appelé lors de la mise à jour d'une identité.
+   * Vérifie les champs "reply-to" et "bcc" et envoie une alerte si une adresse externe est détectée.
+   *
+   * @param array $args Arguments du hook.
+   * @return array Arguments éventuellement modifiés.
+   */
   public function hook_identity_update($args) {
     $RECORD = 'record';
 
@@ -43,6 +89,14 @@ class mel_supervision extends bnum_plugin {
     return $args;
   }
 
+  /**
+   * Envoie un mail d'avertissement si une adresse externe est détectée dans le champ surveillé.
+   * Sauvegarde la modification pour éviter les alertes répétées.
+   *
+   * @param string $key Nom du champ surveillé.
+   * @param \LibMelanie\Api\Defaut\User|null $item Utilisateur correspondant à l'adresse.
+   * @return self
+   */
   private function _sendMessageIfExternal(string $key, ?\LibMelanie\Api\Defaut\User $item): self {
     $VAR_EMAIL = 'email';
     $VAR_KEY = 'key';

114a54034091b57534c6bbf0384f2ea01b651b2f
dduveau <dduveau>
2026-01-14 17:26:40 +0100

    refactor(workspace): :globe_with_meridians: localization

--- plugins/mel_workspace/js/params.js
+++ plugins/mel_workspace/js/params.js
@@ -1260,7 +1260,7 @@
                   rcmail.env.current_workspace_services_actives["tchap-channel"].id = value;
                   $popup.dialog('close');
                 } else {
-                  alert("l'identifiant entré ne correspond pas à un salon tchap");
+                  alert(rcmail.gettext('tchap_id_doesnt_match', 'mel_workspace'));
                 }
               },
             );
@@ -1269,8 +1269,8 @@
       ];
 
       let $popup = rcmail.show_popup_dialog(
-        ' <span> Attention! La présence de Bot-Gmcd [Developpement-Durable] dans le salon est nécessaire pour fonctionner.</span> <br/> <input id = "selecttchapchannel" type = "text" title = "Entrer l\'identifiant interne du salon tchap" /> ',
-        "Entrer l'identifiant interne du salon tchap",
+        '<span> ' + rcmail.gettext('tchap_bot_needed', 'mel_workspace') + '</span> <br/> <input id = "selecttchapchannel" type = "text" title = ' + rcmail.gettext('enter_tchap_id', 'mel_workspace') + '/> ',
+        rcmail.gettext('enter_tchap_id', 'mel_workspace'),
         buttons,
       );
     }

966cb637bb886611600f044ebc80d57c253c9bfa
tdelphin <tdelphin>
2026-04-30 17:04:42 +0200

    feat(app): :sparkles: Ajout d'une regex pour gérer les numéro de tel

--- plugins/mel_metapage/js/lib/constants/regexp.js
+++ plugins/mel_metapage/js/lib/constants/regexp.js
@@ -42,6 +42,7 @@ export const ISO_FORMAT_REGEX = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/;
 export const REG_MAILTO = /^mailto:/;
 export const REG_MAIL_NAME_GLOBAL = /(^<|>$)/g;
 export const REG_LIGHT_PICTURE_NAME = /(-light)\.(([\w\d]+)|\1?.+)$/;
+export const REG_PHONE_NUMBER = /\+?[1-9]\d{1,14}\b/;
 
 /**
  * RegEx qui permet d'extraire la date (YYYY-MM-DD) depuis un timestamp PostgreSQL

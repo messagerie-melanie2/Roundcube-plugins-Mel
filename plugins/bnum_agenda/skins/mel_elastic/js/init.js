631dbfc48159c6df33f42c35f9304b55853f4bfe
tdelphin <tdelphin>
2026-05-28 14:38:30 +0200

    refactor(agenda): :construction: COntinuer

--- plugins/bnum_agenda/skins/mel_elastic/js/init.js
+++ plugins/bnum_agenda/skins/mel_elastic/js/init.js
@@ -1,4 +1,8 @@
-import { HTMLBnumSelect } from '../../../../../skins/mel_elastic/design-system/ds-module-bnum';
+import {
+  ButtonVariation,
+  HTMLBnumSecondaryButton,
+  HTMLBnumSelect,
+} from '../../../../../skins/mel_elastic/design-system/ds-module-bnum.js';
 import ABaseMelObject from '../../../../mel_metapage/js/lib/base_mel_object.js';
 
 /**
@@ -31,7 +35,82 @@ export class ModuleInit extends ABaseMelObject {
    */
   constructor() {
     super();
-    this.#_addListener();
+    this.#_init().#_addListener();
+  }
+
+  async onDocumentReady() {
+    await this.wait_something(() =>
+      document.querySelector('#calendar > .fc-toolbar > .fc-right'),
+    );
+    this.#_initRightButtons();
+  }
+
+  #_init() {
+    return this;
+  }
+
+  #_initRightButtons() {
+    const group = document.createElement('div');
+    group.setAttributeNS('bnum-agenda', 'id', 'ba-group');
+    group.classList.add('group-button');
+
+    const prev = this.#_createRButton('.fc-prev-button', {
+      icon: 'chevron_left',
+    });
+    const today = this.#_createRButton('.fc-today-button', {
+      text:
+        document.querySelector('.fc-today-button')?.textContent ??
+        this.getLocalization('today', { plugin: 'calendar' }),
+    });
+    const next = this.#_createRButton('.fc-next-button', {
+      icon: 'chevron_right',
+    });
+
+    group.append(prev, today, next);
+
+    const r = document.querySelector('#calendar > .fc-toolbar > .fc-right');
+
+    if (!r) throw new Error('Impossible de trouver le header !');
+
+    r.appendChild(group);
+
+    return this;
+  }
+
+  /**
+   *
+   * @param {string} originalSelector
+   * @param {Object} [options={}]
+   * @param {?string} [options.text=null]
+   * @param {?string} [options.icon=null]
+   * @return {HTMLBnumSecondaryButton}
+   */
+  #_createRButton(originalSelector, { text = null, icon = null } = {}) {
+    if (!text && !icon)
+      throw new Error('Un text ou une icône doivent être défini !');
+    if (text && icon)
+      throw new Error(
+        'Un text ou une icône doivent être défini, pas les deux en même temps !',
+      );
+
+    const node = text
+      ? HTMLBnumSecondaryButton.Create({ text, rounded: false })
+      : HTMLBnumSecondaryButton.CreateOnlyIcon(icon, {
+          variation: ButtonVariation.SECONDARY,
+          rounded: true,
+        });
+
+    node.addClass('navigation-button').addEventListener(
+      'click',
+      function (selector) {
+        const elementToClick = document.querySelector(selector);
+
+        if (elementToClick) elementToClick.click();
+        else throw new Error(`Ìmpossible de clicker sur ${selector}`);
+      }.bind(this, originalSelector),
+    );
+
+    return node;
   }
 
   /**
@@ -40,6 +119,7 @@ export class ModuleInit extends ABaseMelObject {
    */
   #_addListener() {
     this.listen(INIT_LISTENER_SELECT, (args) => this.#_initSelectHandler(args));
+    return this;
   }
 
   /**

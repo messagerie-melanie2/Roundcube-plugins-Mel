943b0799d49b044d06e0312f82f0467bd3850380
tdelphin <tdelphin>
2026-05-28 14:38:30 +0200

    refactor(agenda): :construction: COntinuer

--- plugins/bnum_agenda/skins/mel_elastic/js/main.js
+++ plugins/bnum_agenda/skins/mel_elastic/js/main.js
@@ -1,18 +1,44 @@
+import ABaseMelObject from '../../../../mel_metapage/js/lib/base_mel_object.js';
 import { ModuleInit } from './init.js';
 import { SearchModule } from './search.js';
 
 const loader = [SearchModule, ModuleInit];
-class AgendaMelElasticMain {
+class AgendaMelElasticMain extends ABaseMelObject {
+  #_callbacksOnDocumentReady = new Set();
+
   constructor() {
-    this.#_load();
+    super();
+    if (this.get_env('task') === 'calendar') this.#_load();
   }
 
   #_load() {
     for (const Module of loader) {
-      new Module();
+      const module = new Module();
+
+      if (
+        module.onDocumentReady &&
+        typeof module.onDocumentReady === 'function'
+      ) {
+        this.#_callbacksOnDocumentReady.add(
+          module.onDocumentReady.bind(module),
+        );
+      }
     }
     loader.length = 0;
   }
+
+  callForDocumentReady() {
+    if (this.#_callbacksOnDocumentReady.size) {
+      for (const fn of this.#_callbacksOnDocumentReady.values()) {
+        fn();
+      }
+    }
+  }
 }
 
-new AgendaMelElasticMain();
+const module = new AgendaMelElasticMain();
+
+document.addEventListener(
+  'DOMContentLoaded',
+  module.callForDocumentReady.bind(module),
+);

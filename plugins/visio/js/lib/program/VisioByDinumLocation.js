3b8c8c0b35a7d1a6c9a551a31e53635f42f9039f
tdelphin <tdelphin>
2026-08-24 10:28:21 +0200

    refactor(agenda): :recycle: Amélioration du code

--- plugins/visio/js/lib/program/VisioByDinumLocation.js
+++ plugins/visio/js/lib/program/VisioByDinumLocation.js
@@ -5,14 +5,46 @@ import { BnumMessage } from '../../../../mel_metapage/js/lib/classes/bnum_messag
 import { EMPTY_STRING } from '../../../../mel_metapage/js/lib/constants/constants.js';
 import { VisioRooms } from './visio_rooms.js';
 
+/**
+ * Active le cache statique de room entre deux ouvertures de la dialog
+ * d'évènement, pour éviter de recréer une room Dinum à chaque fois.
+ * @type {boolean}
+ */
 const ENABLE_SECURE = true;
 
+/**
+ * Partie de localisation d'un évènement représentant une visioconférence
+ * créée via l'API Dinum (VisioRooms).
+ *
+ * La création de la room est asynchrone : le bouton "save" de la dialog
+ * d'évènement est désactivé le temps de la création pour empêcher
+ * l'enregistrement d'un évènement sans room valide.
+ * @extends {AVisio}
+ */
 export class VisioByDinumLocation extends AVisio {
-  #_createButton({ secure }) {
-    const { name: cacheName, location: cacheLocation } =
-      VisioByDinumLocation._visioData || {};
-    const location = secure ? cacheLocation || this.location : this.location;
-    const name = secure ? cacheName || this._name : this._name;
+  /**
+   * Dernière room créée, mise en cache au niveau de la classe tant que
+   * {@link ENABLE_SECURE} est actif, pour la réutiliser sans rappeler l'API
+   * si la dialog est rouverte.
+   * @type {{name: string, location: string}|null}
+   */
+  static #_visioData = null;
+
+  /**
+   * Construit le bouton (désactivé) affichant le nom de la room.
+   *
+   * Si {@link ENABLE_SECURE} est actif et qu'une room est déjà en cache, son
+   * nom/emplacement est utilisé pour éviter un état "loading" superflu.
+   * @returns {HTMLElement} Bouton en lecture seule prêt à être inséré dans le DOM
+   * @private
+   */
+  #_createButton() {
+    const { name: cachedName, location: cachedLocation } =
+      VisioByDinumLocation.#_visioData || {};
+    const location = ENABLE_SECURE
+      ? cachedLocation || this.location
+      : this.location;
+    const name = ENABLE_SECURE ? cachedName || this._name : this._name;
     const btn = HTMLBnumButton.Create({
       text: name ? `Room : ${name}` : EMPTY_STRING,
       loading: !location,
@@ -23,93 +55,130 @@ export class VisioByDinumLocation extends AVisio {
     return btn;
   }
 
+  /**
+   * Active ou désactive le bouton "save" de la dialog d'évènement.
+   *
+   * Encapsule la différence entre la dialog jQuery UI et la dialog "custom"
+   * ({@link EventView#is_jquery_dialog}) pour que le reste de la classe n'ait
+   * à connaître qu'un état booléen, pas la structure interne de la dialog.
+   * @param {boolean} enabled `true` pour réactiver le bouton, `false` pour le désactiver
+   * @private
+   */
+  #_setSaveButtonEnabled(enabled) {
+    const dialog = EventView.INSTANCE.get_dialog();
+    const saveButton = EventView.INSTANCE.is_jquery_dialog()
+      ? dialog.parent().find('.ui-dialog-buttonset .mainaction')
+      : dialog.footer.buttons.save;
+
+    if (enabled) {
+      saveButton.removeAttr('disabled').removeClass('disabled');
+    } else {
+      saveButton.attr('disabled', 'disabled').addClass('disabled');
+    }
+  }
+
+  /**test
+   * Crée une nouvelle room Dinum via {@link VisioRooms}, met à jour le
+   * bouton et l'emplacement de l'évènement, puis réactive le bouton "save".
+   * @async
+   * @returns {Promise<void>}
+   * @private
+   */
+  async #_createRoom() {
+    const loading = BnumMessage.DisplayLoadingMessage();
+    const rooms = new VisioRooms();
+    const { datas } = await rooms.createRoom();
+    const { content } = datas;
+    const { url, name, telephony } = content;
+    const { phone_number, pin_code } = telephony;
+
+    this._name = name;
+    this.location = `${url} (${phone_number} | #${pin_code})`;
+
+    if (ENABLE_SECURE) {
+      VisioByDinumLocation.#_visioData = { name, location: this.location };
+    }
+
+    this.btn.innerText = `Room : ${this._name}`;
+    this.btn.stopLoading();
+    this.#_setSaveButtonEnabled(true);
+
+    BnumMessage.ClearMessage(loading);
+    queueMicrotask(() => this.onchange.call());
+  }
+
+  /**
+   * Construit le DOM de la partie de localisation, l'insère dans `$parent`,
+   * puis résout l'emplacement de la room : réutilisation du cache si
+   * disponible, sinon création d'une nouvelle room via {@link VisioRooms}.
+   * @param {JQuery} $parent Conteneur dans lequel insérer la partie de localisation
+   */
   generate($parent) {
-    const { location: cacheLocation } = VisioByDinumLocation._visioData || {};
+    const btn = this.#_createButton();
 
-    const btn = this.#_createButton({ secure: ENABLE_SECURE });
-
-    this.btn = btn;
     const center = document.createElement('center');
     center.appendChild(btn);
+    this.btn = btn;
+
     const div = document.createElement('div');
-    div.classList.add('location-mode');
+    div.classList.add('visio-mode');
     div.setAttribute('data-locationmode', this.option_value());
     div.appendChild(center);
 
     $parent.append(div);
+    this.btn = btn;
 
-    let dialog = EventView.INSTANCE.get_dialog();
-    if (EventView.INSTANCE.is_jquery_dialog()) {
-      dialog
-        .parent()
-        .find('.ui-dialog-buttonset .mainaction')
-        .attr('disabled', 'disabled')
-        .addClass('disabled');
-    } else {
-      dialog.footer.buttons.save
-        .attr('disabled', 'disabled')
-        .addClass('disabled');
-    }
+    this.#_setSaveButtonEnabled(false);
 
-    if (ENABLE_SECURE && cacheLocation) {
-      this.location = cacheLocation;
+    const { location: cachedLocation } = VisioByDinumLocation.#_visioData || {};
+    if (ENABLE_SECURE && cachedLocation) {
+      this.location = cachedLocation;
       queueMicrotask(() => this.onchange.call());
       return;
     }
 
-    const loading = BnumMessage.DisplayLoadingMessage();
-    const rooms = new VisioRooms();
-    this.promise = rooms.createRoom().then((val) => {
-      const { datas } = val;
-      const { content } = datas;
-      const { url, name, telephony } = content;
-      const { phone_number, pin_code } = telephony;
-
-      this._name = name;
-      this.location = `${url} (${phone_number} | #${pin_code})`;
-
-      if (ENABLE_SECURE)
-        VisioByDinumLocation._visioData = { name, location: this.location };
-
-      this.btn.innerText = `Room : ${this._name}`;
-      this.btn.stopLoading();
-
-      if (EventView.INSTANCE.is_jquery_dialog()) {
-        dialog
-          .parent()
-          .find('.ui-dialog-buttonset .mainaction')
-          .removeAttr('disabled')
-          .removeClass('disabled');
-      } else {
-        dialog.footer.buttons.save
-          .removeAttr('disabled')
-          .removeClass('disabled');
-      }
-
-      BnumMessage.ClearMessage(loading);
-      queueMicrotask(() => this.onchange.call());
-    });
+    this.promise = this.#_createRoom();
   }
 
+  /**
+   * Une room Dinum est toujours considérée comme valide une fois générée.
+   * @returns {boolean}
+   */
   is_valid() {
     return true;
   }
 
+  /**
+   * Attend la fin de la création de la room en cours, s'il y en a une.
+   * @async
+   * @returns {Promise<void>}
+   */
   async wait() {
     await (this.promise || Promise.resolve());
   }
 
+  /**
+   * Réinitialise le cache de room et l'état interne de l'instance.
+   */
   destroy() {
     super.destroy();
 
-    VisioByDinumLocation._visioData = null;
+    VisioByDinumLocation.#_visioData = null;
     this.promise = null;
   }
 
+  /**
+   * Valeur de l'option de localisation correspondant à ce type de visio.
+   * @returns {string}
+   */
   static OptionValue() {
     return 'dinum';
   }
 
+  /**
+   * Plugin fournissant ce type de visio.
+   * @returns {string}
+   */
   static PluginName() {
     return 'visio';
   }
